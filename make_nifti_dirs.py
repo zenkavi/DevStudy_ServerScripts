d90a927cd84ce11d889e27917d1e4908d4d8ecf3
Zeynep Enkavi <zeynepenkavi@gmail.com>
2018-05-22 14:25:17 -0700

    refactoring

--- make_nifti_dirs.py
+++
@@ -1,4 +0,0 @@
-import os
-dicom_dirs = os.listdir('/corral-repl/utexas/poldracklab/users/zenkavi/dev_study/02_DICOM_data_after_unzipping/')
-for i in range(len(dicom_dirs)):
-    os.mkdir('/corral-repl/utexas/poldracklab/users/zenkavi/dev_study/03_DICOM_to_NIFTI_conversions/'+dicom_dirs[i])
