957da9b80f201ff9752722b6911b52b7846ec7a5
Zeynep Enkavi <zeynepenkavi@gmail.com>
2018-05-23 12:00:48 -0700

    refactoring

--- setup/dev_study_env.sh
+++ setup/dev_study_env.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 module load dcm2niix/7July2016
-module load anaconda/4.1.1-py2
+#module load anaconda/4.1.1-py2
 module load Rstats/3.2.1
 module load fsl/5.0.9
 module load fslview/5.0.9
 #module load launch
 module load launcher
+source activate dev_study
