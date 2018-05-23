f042bac8ed320fe20ef4912ff991f10d9b5f892a
Zeynep Enkavi <zeynepenkavi@gmail.com>
2018-05-23 12:38:55 -0700

    added script to run validator

---
+++ bidsify/run_bids_validator.sh
@@ -0,0 +1,3 @@
+module load system
+module load singularity
+singularity run /share/PI/russpold/singularity_images/bids_validator-2018-05-19-505df799730d.img /oak/stanford/groups/russpold/data/ds000054/0.0.1/
