8483e702609895af28283427983782aa56101c68
Zeynep Enkavi <zeynepenkavi@gmail.com>
2018-05-24 16:18:35 -0700

    debug setup script

--- setup/dev_study_env.sh
+++ setup/dev_study_env.sh
@@ -1,10 +1,9 @@
 # change paths and modules depending on cluster
 # sherlock
 if [ $HOME == '/home/users/zenkavi' ]; then
-  export PATH="/home1/04127/zenkavi/miniconda3/bin:$PATH"
   echo "Working"
-  # export PATH=$HOME/miniconda/bin:$PATH
-  # export TODO_PATH=/oak/stanford/groups/russpold/data/ds000054/todo/
+  export PATH=$HOME/miniconda/bin:$PATH
+  export TODO_PATH=/oak/stanford/groups/russpold/data/ds000054/todo/
   # export SERVER_SCRIPTS=/home/users/zenkavi/research/DevStudy_ServerScripts/
   # export DATA_LOC=/oak/stanford/groups/russpold/data/ds000054/0.0.1/
 # tacc
@@ -12,7 +11,7 @@ else
   export PATH="/home1/04127/zenkavi/miniconda3/bin:$PATH"
   echo "Not working"
   # source activate dev_study
-  # export TODO_PATH=/corral-repl/utexas/poldracklab/users/zenkavi/dev_study/todo/
+  export TODO_PATH=/corral-repl/utexas/poldracklab/users/zenkavi/dev_study/todo/
   # export SERVER_SCRIPTS=/corral-repl/utexas/poldracklab/users/zenkavi/dev_study/DevStudy_ServerScripts/
   # export DATA_LOC=/corral-repl/utexas/poldracklab/users/zenkavi/dev_study/data/
   # module load dcm2niix/7July2016
