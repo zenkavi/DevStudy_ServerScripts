685aac32325a82b9e940cc9d85af8978173b5c21
Zeynep Enkavi <zeynepenkavi@gmail.com>
2018-05-24 17:17:15 -0700

    debug setup script

--- setup/dev_study_env.sh
+++ setup/dev_study_env.sh
@@ -1,19 +1,17 @@
 # change paths and modules depending on cluster
 # sherlock
 if [ $HOME == '/home/users/zenkavi' ]; then
-  echo "Working";
-  export PATH=$HOME/miniconda/bin:$PATH;
-  export TODO_PATH=/oak/stanford/groups/russpold/data/ds000054/todo/;
-  # export SERVER_SCRIPTS=/home/users/zenkavi/research/DevStudy_ServerScripts/
-  # export DATA_LOC=/oak/stanford/groups/russpold/data/ds000054/0.0.1/
+  source activate dev_study
+  export TODO_PATH=/oak/stanford/groups/russpold/data/ds000054/todo/
+  export SERVER_SCRIPTS=/home/users/zenkavi/research/DevStudy_ServerScripts/
+  export DATA_LOC=/oak/stanford/groups/russpold/data/ds000054/0.0.1/
 # tacc
 else
-  export PATH="/home1/04127/zenkavi/miniconda3/bin:$PATH"
-  echo "Not working";
-  # source activate dev_study
-  export TODO_PATH=/corral-repl/utexas/poldracklab/users/zenkavi/dev_study/todo/;
-  # export SERVER_SCRIPTS=/corral-repl/utexas/poldracklab/users/zenkavi/dev_study/DevStudy_ServerScripts/
-  # export DATA_LOC=/corral-repl/utexas/poldracklab/users/zenkavi/dev_study/data/
+  source activate dev_study
+  export TODO_PATH=/corral-repl/utexas/poldracklab/users/zenkavi/dev_study/todo/
+  export SERVER_SCRIPTS=/corral-repl/utexas/poldracklab/users/zenkavi/dev_study/DevStudy_ServerScripts/
+  export DATA_LOC=/corral-repl/utexas/poldracklab/users/zenkavi/dev_study/data/
+  export R_LIB=/work/04127/zenkavi/.r_library/
   # module load dcm2niix/7July2016
   # module load Rstats/3.2.1
 fi
