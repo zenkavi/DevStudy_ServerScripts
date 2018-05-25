a5df38f4ec4f1994dbb69761122b871d07610515
Zeynep Enkavi <zeynepenkavi@gmail.com>
2018-05-25 13:28:47 -0700

    refactoring

--- bidsify/make_commands_for_bart_conversion.py
+++ bidsify/make_commands_for_bart_conversion.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import os
 
-mat_file_names = os.listdir("/corral-repl/utexas/poldracklab/users/zenkavi/dev_study/todo/behav_data_tb_organized/bart")
+mat_file_names = os.listdir(todo_path+"behav_data_tb_organized/bart")
 
 sub_ids = [ x.split("_")[0] for x in mat_file_names ]
 
@@ -9,10 +9,8 @@ tsv_file_names = [ x.split(".")[0]+(".tsv") for x in mat_file_names]
 
 json_file_names = [ x.split(".")[0]+(".json") for x in mat_file_names]
 
-print('#!/bin/bash')
-
 for i in range(len(mat_file_names)):
-	command = 'Rscript --vanilla /corral-repl/utexas/poldracklab/users/zenkavi/dev_study/DevStudy_ServerScripts/bidsify/convertBARTmat2tsv.R /corral-repl/utexas/poldracklab/users/zenkavi/dev_study/todo/behav_data_tb_organized/bart/' + mat_file_names[i] + ' /corral-repl/utexas/poldracklab/users/zenkavi/dev_study/data/sub-' + sub_ids[i] + '/beh/' + tsv_file_names[i] + ' /corral-repl/utexas/poldracklab/users/zenkavi/dev_study/data/sub-'+sub_ids[i]+'/beh/'+ json_file_names[i]
+	command = 'Rscript --vanilla $SERVER_SCRIPTS/bidsify/convertBARTmat2tsv.R $TODO_PATH/behav_data_tb_organized/bart/' + mat_file_names[i] + ' $DATA_LOC/sub-' + sub_ids[i] + '/beh/' + tsv_file_names[i] + ' $DATA_LOC/sub-'+sub_ids[i]+'/beh/'+ json_file_names[i]
 	print(command)
 
 #sub-<participant_label>[_ses-<session_label>]_task-<task_name>_beh.tsv
--- setup/dev_study_env.sh
+++ setup/dev_study_env.sh
@@ -2,17 +2,17 @@
 # sherlock
 if [ $HOME == '/home/users/zenkavi' ]; then
   source activate dev_study
-  export TODO_PATH=/oak/stanford/groups/russpold/data/ds000054/todo/
-  export SERVER_SCRIPTS=/home/users/zenkavi/research/DevStudy_ServerScripts/
-  export DATA_LOC=/oak/stanford/groups/russpold/data/ds000054/0.0.1/
-  export R_LIB=/home/users/zenkavi/miniconda/envs/dev_study/lib/R/library/
+  export TODO_PATH=/oak/stanford/groups/russpold/data/ds000054/todo
+  export SERVER_SCRIPTS=/home/users/zenkavi/research/DevStudy_ServerScripts
+  export DATA_LOC=/oak/stanford/groups/russpold/data/ds000054/0.0.1
+  export R_LIB=/home/users/zenkavi/miniconda/envs/dev_study/lib/R/library
 # tacc
 else
   source activate dev_study
-  export TODO_PATH=/corral-repl/utexas/poldracklab/users/zenkavi/dev_study/todo/
-  export SERVER_SCRIPTS=/corral-repl/utexas/poldracklab/users/zenkavi/dev_study/DevStudy_ServerScripts/
-  export DATA_LOC=/corral-repl/utexas/poldracklab/users/zenkavi/dev_study/data/
-  export R_LIB=/work/04127/zenkavi/.r_library/
+  export TODO_PATH=/corral-repl/utexas/poldracklab/users/zenkavi/dev_study/todo
+  export SERVER_SCRIPTS=/corral-repl/utexas/poldracklab/users/zenkavi/dev_study/DevStudy_ServerScripts
+  export DATA_LOC=/corral-repl/utexas/poldracklab/users/zenkavi/dev_study/data
+  export R_LIB=/work/04127/zenkavi/.r_library
   # module load dcm2niix/7July2016
   # module load Rstats/3.2.1
 fi
