260b29a214c9338617e250f16ba690a76c60b7aa
Zeynep Enkavi <zeynepenkavi@gmail.com>
2018-05-25 15:00:57 -0700

    testing batch script

--- bidsify/make_commands_for_bart_conversion.py
+++ bidsify/make_commands_for_bart_conversion.py
@@ -8,5 +8,5 @@ mat_file_names = os.listdir(todo_path+"/behav_data_tb_organized/bart")
 sub_ids = [ x.split("_")[0] for x in mat_file_names ]
 
 for i in range(len(mat_file_names)):
-	command = 'Rscript --vanilla $SERVER_SCRIPTS/bidsify/convertBARTmat2tsv.R $TODO_PATH/behav_data_tb_organized/bart/' + mat_file_names[i] + ' $DATA_LOC/sub-' + sub_ids[i] + '/beh/sub-' + sub_ids[i]+ '_task-bart_beh'
+	command = 'Rscript --vanilla $SERVER_SCRIPTS/bidsify/convertBARTmat2tsv.R {TODO_PATH}/behav_data_tb_organized/bart/' + mat_file_names[i] + ' {DATA_LOC}/sub-' + sub_ids[i] + '/beh/sub-' + sub_ids[i]+ '_task-bart_beh'
 	print(command)
--- bidsify/test_task_list_bart_mat_to_tsv.sh
+++ bidsify/test_task_list_bart_mat_to_tsv.sh
@@ -1,2 +1,2 @@
-Rscript --vanilla $SERVER_SCRIPTS/bidsify/convertBARTmat2tsv.R $TODO_PATH/behav_data_tb_organized/bart/400742_bart_behav_21-Apr-2014_10-45.mat $DATA_LOC/sub-400742/beh/sub-400742_task-bart_beh
-Rscript --vanilla $SERVER_SCRIPTS/bidsify/convertBARTmat2tsv.R $TODO_PATH/behav_data_tb_organized/bart/200056_bart_behav_22-Mar-2014_11-09.mat $DATA_LOC/sub-200056/beh/sub-200056_task-bart_beh
+Rscript --vanilla {SERVER_SCRIPTS}/bidsify/convertBARTmat2tsv.R {TODO_PATH}/behav_data_tb_organized/bart/400742_bart_behav_21-Apr-2014_10-45.mat {DATA_LOC}/sub-400742/beh/sub-400742_task-bart_beh
+Rscript --vanilla {SERVER_SCRIPTS}/bidsify/convertBARTmat2tsv.R {TODO_PATH}/behav_data_tb_organized/bart/200056_bart_behav_22-Mar-2014_11-09.mat {DATA_LOC}/sub-200056/beh/sub-200056_task-bart_beh
