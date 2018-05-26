7fc0dddde0e2ae7435dc90e5a4403757ae64e959
Zeynep Enkavi <zeynepenkavi@gmail.com>
2018-05-25 19:23:17 -0700

    testing batch script

--- bidsify/test_task_list_bart_mat_to_tsv.sh
+++ bidsify/test_task_list_bart_mat_to_tsv.sh
@@ -1,2 +1,4 @@
 Rscript --vanilla $SERVER_SCRIPTS/bidsify/convertBARTmat2tsv.R $TODO_PATH/behav_data_tb_organized/bart/400742_bart_behav_21-Apr-2014_10-45.mat $DATA_LOC/sub-400742/beh/sub-400742_task-bart_beh
 Rscript --vanilla $SERVER_SCRIPTS/bidsify/convertBARTmat2tsv.R $TODO_PATH/behav_data_tb_organized/bart/200056_bart_behav_22-Mar-2014_11-09.mat $DATA_LOC/sub-200056/beh/sub-200056_task-bart_beh
+Rscript --vanilla $SERVER_SCRIPTS/bidsify/convertBARTmat2tsv.R $TODO_PATH/behav_data_tb_organized/bart/200133_bart_behav_25-Jun-2014_18-09.mat $DATA_LOC/sub-200133/beh/sub-200133_task-bart_beh
+Rscript --vanilla $SERVER_SCRIPTS/bidsify/convertBARTmat2tsv.R $TODO_PATH/behav_data_tb_organized/bart/200166_bart_behav_12-Jun-2014_18-02.mat $DATA_LOC/sub-200166/beh/sub-200166_task-bart_beh
