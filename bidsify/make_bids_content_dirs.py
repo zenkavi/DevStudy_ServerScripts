72f3178f8e197c17b22d9df06969314bd1d90414
Zeynep Enkavi <zeynepenkavi@gmail.com>
2018-05-25 19:16:02 -0700

    testing batch script

--- bidsify/make_bids_content_dirs.py
+++ bidsify/make_bids_content_dirs.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
 import os
 
-dir_names = open("/corral-repl/utexas/poldracklab/users/zenkavi/dev_study/DevStudy_ServerScripts/bidsify/sub_dirname_list.txt")
+server_scripts = os.environ['SERVER_SCRIPTS']
+data_loc = os.environ['DATA_LOC']
+
+dir_names = open(server_scripts+"/bidsify/sub_dirname_list.txt")
 
 for line in dir_names:
-	os.mkdir("/corral-repl/utexas/poldracklab/users/zenkavi/dev_study/data/sub-"+line.strip()+"/anat")
-	os.mkdir("/corral-repl/utexas/poldracklab/users/zenkavi/dev_study/data/sub-"+line.strip()+"/func")
-	os.mkdir("/corral-repl/utexas/poldracklab/users/zenkavi/dev_study/data/sub-"+line.strip()+"/fmap")
-	os.mkdir("/corral-repl/utexas/poldracklab/users/zenkavi/dev_study/data/sub-"+line.strip()+"/beh")
+	os.mkdir(data_loc+"/sub-"+line.strip()+"/anat")
+	os.mkdir(data_loc+"/sub-"+line.strip()+"/func")
+	os.mkdir(data_loc+"/sub-"+line.strip()+"/fmap")
+	os.mkdir(data_loc+"/sub-"+line.strip()+"/beh")
 
 dir_names.close()
