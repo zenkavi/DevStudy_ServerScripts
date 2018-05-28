cbea940bac6fd6d167ffd98ccfcec1229bfd4a46
Zeynep Enkavi <zeynepenkavi@gmail.com>
2018-05-28 11:43:07 -0700

    added manual corrections for bidsifying

---
+++ bidsify/manual_corrections.sh
@@ -0,0 +1,128 @@
+#sub-405027 corrections
+mv sub-405027_2/ ./sub-405027
+rename sub-405027_2 sub-405027 *
+
+#sub-407260 corrections: same as sub-407260
+rm -rf sub-407260_b
+
+#remove test scans
+rm -rf sub-test
+
+#replace 200067 with 200027 in participants.tsv
+
+#change multiple fmaps to runs
+mv sub-100009_magnitude1.nii.gz ./sub-100009_run-1_magnitude1.nii.gz
+mv sub-100009_magnitude1.json ./sub-100009_run-1_magnitude1.json
+mv sub-100009_magnitude2.nii.gz ./sub-100009_run-1_magnitude2.nii.gz
+mv sub-100009_magnitude2.json ./sub-100009_run-1_magnitude2.json
+mv sub-100009_magnitude3.nii.gz ./sub-100009_run-2_magnitude1.nii.gz
+mv sub-100009_magnitude3.json ./sub-100009_run-2_magnitude1.json
+mv sub-100009_magnitude4.nii.gz ./sub-100009_run-2_magnitude2.nii.gz
+mv sub-100009_magnitude4.json ./sub-100009_run-2_magnitude2.json
+mv sub-100009_phasediff1.nii.gz ./sub-100009_run-1_phasediff.nii.gz
+mv sub-100009_phasediff1.json ./sub-100009_run-1_phasediff.json
+mv sub-100009_phasediff2.nii.gz ./sub-100009_run-2_phasediff.nii.gz
+mv sub-100009_phasediff2.json ./sub-100009_run-2_phasediff.json
+
+mv sub-100051_magnitude1.nii.gz ./sub-100051_run-1_magnitude1.nii.gz
+mv sub-100051_magnitude1.json ./sub-100051_run-1_magnitude1.json
+mv sub-100051_magnitude2.nii.gz ./sub-100051_run-1_magnitude2.nii.gz
+mv sub-100051_magnitude2.json ./sub-100051_run-1_magnitude2.json
+mv sub-100051_magnitude3.nii.gz ./sub-100051_run-2_magnitude1.nii.gz
+mv sub-100051_magnitude3.json ./sub-100051_run-2_magnitude1.json
+mv sub-100051_magnitude4.nii.gz ./sub-100051_run-2_magnitude2.nii.gz
+mv sub-100051_magnitude4.json ./sub-100051_run-2_magnitude2.json
+mv sub-100051_phasediff1.nii.gz ./sub-100051_run-1_phasediff.nii.gz
+mv sub-100051_phasediff1.json ./sub-100051_run-1_phasediff.json
+mv sub-100051_phasediff2.nii.gz ./sub-100051_run-2_phasediff.nii.gz
+mv sub-100051_phasediff2.json ./sub-100051_run-2_phasediff.json
+
+mv sub-100062_magnitude1.nii.gz ./sub-100062_run-1_magnitude1.nii.gz
+mv sub-100062_magnitude1.json ./sub-100062_run-1_magnitude1.json
+mv sub-100062_magnitude2.nii.gz ./sub-100062_run-1_magnitude2.nii.gz
+mv sub-100062_magnitude2.json ./sub-100062_run-1_magnitude2.json
+mv sub-100062_magnitude3.nii.gz ./sub-100062_run-2_magnitude1.nii.gz
+mv sub-100062_magnitude3.json ./sub-100062_run-2_magnitude1.json
+mv sub-100062_magnitude4.nii.gz ./sub-100062_run-2_magnitude2.nii.gz
+mv sub-100062_magnitude4.json ./sub-100062_run-2_magnitude2.json
+mv sub-100062_magnitude5.nii.gz ./sub-100062_run-3_magnitude1.nii.gz
+mv sub-100062_magnitude5.json ./sub-100062_run-3_magnitude1.json
+mv sub-100062_magnitude6.nii.gz ./sub-100062_run-3_magnitude2.nii.gz
+mv sub-100062_magnitude6.json ./sub-100062_run-3_magnitude2.json
+mv sub-100062_phasediff1.nii.gz ./sub-100062_run-1_phasediff.nii.gz
+mv sub-100062_phasediff1.json ./sub-100062_run-1_phasediff.json
+mv sub-100062_phasediff2.nii.gz ./sub-100062_run-2_phasediff.nii.gz
+mv sub-100062_phasediff2.json ./sub-100062_run-2_phasediff.json
+mv sub-100062_phasediff3.nii.gz ./sub-100062_run-3_phasediff.nii.gz
+mv sub-100062_phasediff3.json ./sub-100062_run-3_phasediff.json
+
+mv sub-100103_magnitude1.nii.gz ./sub-100103_run-1_magnitude1.nii.gz
+mv sub-100103_magnitude1.json ./sub-100103_run-1_magnitude1.json
+mv sub-100103_magnitude2.nii.gz ./sub-100103_run-1_magnitude2.nii.gz
+mv sub-100103_magnitude2.json ./sub-100103_run-1_magnitude2.json
+mv sub-100103_magnitude3.nii.gz ./sub-100103_run-2_magnitude1.nii.gz
+mv sub-100103_magnitude3.json ./sub-100103_run-2_magnitude1.json
+mv sub-100103_magnitude4.nii.gz ./sub-100103_run-2_magnitude2.nii.gz
+mv sub-100103_magnitude4.json ./sub-100103_run-2_magnitude2.json
+mv sub-100103_phasediff1.nii.gz ./sub-100103_run-1_phasediff.nii.gz
+mv sub-100103_phasediff1.json ./sub-100103_run-1_phasediff.json
+mv sub-100103_phasediff2.nii.gz ./sub-100103_run-2_phasediff.nii.gz
+mv sub-100103_phasediff2.json ./sub-100103_run-2_phasediff.json
+
+mv sub-100188_magnitude1.nii.gz ./sub-100188_run-1_magnitude1.nii.gz
+mv sub-100188_magnitude1.json ./sub-100188_run-1_magnitude1.json
+mv sub-100188_magnitude2.nii.gz ./sub-100188_run-1_magnitude2.nii.gz
+mv sub-100188_magnitude2.json ./sub-100188_run-1_magnitude2.json
+mv sub-100188_magnitude3.nii.gz ./sub-100188_run-2_magnitude1.nii.gz
+mv sub-100188_magnitude3.json ./sub-100188_run-2_magnitude1.json
+mv sub-100188_magnitude4.nii.gz ./sub-100188_run-2_magnitude2.nii.gz
+mv sub-100188_magnitude4.json ./sub-100188_run-2_magnitude2.json
+mv sub-100188_phasediff1.nii.gz ./sub-100188_run-1_phasediff.nii.gz
+mv sub-100188_phasediff1.json ./sub-100188_run-1_phasediff.json
+mv sub-100188_phasediff2.nii.gz ./sub-100188_run-2_phasediff.nii.gz
+mv sub-100188_phasediff2.json ./sub-100188_run-2_phasediff.json
+
+mv sub-100241_magnitude1.nii.gz ./sub-100241_run-1_magnitude1.nii.gz
+mv sub-100241_magnitude1.json ./sub-100241_run-1_magnitude1.json
+mv sub-100241_magnitude2.nii.gz ./sub-100241_run-1_magnitude2.nii.gz
+mv sub-100241_magnitude2.json ./sub-100241_run-1_magnitude2.json
+mv sub-100241_magnitude3.nii.gz ./sub-100241_run-2_magnitude1.nii.gz
+mv sub-100241_magnitude3.json ./sub-100241_run-2_magnitude1.json
+mv sub-100241_magnitude4.nii.gz ./sub-100241_run-2_magnitude2.nii.gz
+mv sub-100241_magnitude4.json ./sub-100241_run-2_magnitude2.json
+mv sub-100241_phasediff1.nii.gz ./sub-100241_run-1_phasediff.nii.gz
+mv sub-100241_phasediff1.json ./sub-100241_run-1_phasediff.json
+mv sub-100241_phasediff2.nii.gz ./sub-100241_run-2_phasediff.nii.gz
+mv sub-100241_phasediff2.json ./sub-100241_run-2_phasediff.json
+
+mv sub-200056_magnitude1.nii.gz ./sub-200056_run-1_magnitude1.nii.gz
+mv sub-200056_magnitude1.json ./sub-200056_run-1_magnitude1.json
+mv sub-200056_magnitude2.nii.gz ./sub-200056_run-1_magnitude2.nii.gz
+mv sub-200056_magnitude2.json ./sub-200056_run-1_magnitude2.json
+mv sub-200056_magnitude3.nii.gz ./sub-200056_run-2_magnitude1.nii.gz
+mv sub-200056_magnitude3.json ./sub-200056_run-2_magnitude1.json
+mv sub-200056_magnitude4.nii.gz ./sub-200056_run-2_magnitude2.nii.gz
+mv sub-200056_magnitude4.json ./sub-200056_run-2_magnitude2.json
+mv sub-200056_phasediff1.nii.gz ./sub-200056_run-1_phasediff.nii.gz
+mv sub-200056_phasediff1.json ./sub-200056_run-1_phasediff.json
+mv sub-200056_phasediff2.nii.gz ./sub-200056_run-2_phasediff.nii.gz
+mv sub-200056_phasediff2.json ./sub-200056_run-2_phasediff.json
+
+mv sub-306065_magnitude1.nii.gz ./sub-306065_run-1_magnitude1.nii.gz
+mv sub-306065_magnitude1.json ./sub-306065_run-1_magnitude1.json
+mv sub-306065_magnitude2.nii.gz ./sub-306065_run-1_magnitude2.nii.gz
+mv sub-306065_magnitude2.json ./sub-306065_run-1_magnitude2.json
+mv sub-306065_magnitude3.nii.gz ./sub-306065_run-2_magnitude1.nii.gz
+mv sub-306065_magnitude3.json ./sub-306065_run-2_magnitude1.json
+mv sub-306065_magnitude4.nii.gz ./sub-306065_run-2_magnitude2.nii.gz
+mv sub-306065_magnitude4.json ./sub-306065_run-2_magnitude2.json
+mv sub-306065_magnitude5.nii.gz ./sub-306065_run-3_magnitude1.nii.gz
+mv sub-306065_magnitude5.json ./sub-306065_run-3_magnitude1.json
+mv sub-306065_magnitude6.nii.gz ./sub-306065_run-3_magnitude2.nii.gz
+mv sub-306065_magnitude6.json ./sub-306065_run-3_magnitude2.json
+mv sub-306065_phasediff1.nii.gz ./sub-306065_run-1_phasediff.nii.gz
+mv sub-306065_phasediff1.json ./sub-306065_run-1_phasediff.json
+mv sub-306065_phasediff2.nii.gz ./sub-306065_run-2_phasediff.nii.gz
+mv sub-306065_phasediff2.json ./sub-306065_run-2_phasediff.json
+mv sub-306065_phasediff3.nii.gz ./sub-306065_run-3_phasediff.nii.gz
+mv sub-306065_phasediff3.json ./sub-306065_run-3_phasediff.json
