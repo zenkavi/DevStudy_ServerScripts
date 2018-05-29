f3dcc7b58fa97f25c38911e7639e2d44b0fb72d8
Zeynep Enkavi <zeynepenkavi@gmail.com>
2018-05-29 13:51:15 -0700

    added script for qc task list

--- mriqc/make_commands_for_mriqc.py
+++ mriqc/make_commands_for_mriqc.py
@@ -7,7 +7,7 @@ data_loc = os.environ['DATA_LOC']
 sub_dirnames = [f for f in os.listdir(data_loc) if re.match(r'sub', f)]
 
 for i in sub_dirnames:
-	command = 'singularity run /share/PI/russpold/singularity_images/poldracklab_mriqc_0.10.4-2018-03-23-f1e4d5941533.img $DATA_LOC/ $DATA_LOC/derivatives/ participant -w $LOCAL_SCRATCH/work --participant_label ' + i + ' --mem-mb 50000 --nthreads 10 --omp-nthreads 8 -vv'
+	command = 'singularity run /share/PI/russpold/singularity_images/poldracklab_mriqc_0.10.5-2018-05-28-9954e8c774c2.img $DATA_LOC/ $DATA_LOC/derivatives/mriqc_0.10.5/ participant -w $LOCAL_SCRATCH/work --participant_label ' + i + ' --mem-gb 50 --n_procs 10 --ants-nthreads 8 -vvv'
 	print(command)
 
-#singularity run /share/PI/russpold/singularity_images/poldracklab_fmriprep_1.0.15-2018-05-17-ad916df843e8.img $OAK/data/ds000054/0.0.1/ $OAK/data/ds000054/0.0.1/derivatives/ participant -w work --participant_label sub-100003 --mem-mb 50000 --nthreads 10 --omp-nthreads 8 -vv
+#ingularity run /share/PI/russpold/singularity_images/poldracklab_mriqc_0.10.5-2018-05-28-9954e8c774c2.img $DATA_LOC/ $DATA_LOC/derivatives/mriqc_0.10.5/ participant -w $LOCAL_SCRATCH/work --participant_label sub-100003 --mem-gb 50 --n_procs 10 --ants-nthreads 8 -vvv
