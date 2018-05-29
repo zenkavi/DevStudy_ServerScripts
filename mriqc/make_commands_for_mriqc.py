dd681e42c6247ed9697b45e0ec1da4e0feb11055
Zeynep Enkavi <zeynepenkavi@gmail.com>
2018-05-29 10:14:29 -0700

    working on qc script

---
+++ mriqc/make_commands_for_mriqc.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+import os
+import re
+
+data_loc = os.environ['DATA_LOC']
+
+sub_dirnames = [f for f in os.listdir(data_loc) if re.match(r'sub', f)]
+
+for i in sub_dirnames:
+	command = 'singularity run /share/PI/russpold/singularity_images/poldracklab_mriqc_0.10.4-2018-03-23-f1e4d5941533.img $DATA_LOC/ $DATA_LOC/derivatives/ participant -w $LOCAL_SCRATCH/work --participant_label ' + i + ' --mem-mb 50000 --nthreads 10 --omp-nthreads 8 -vv'
+	print(command)
+
+#singularity run /share/PI/russpold/singularity_images/poldracklab_fmriprep_1.0.15-2018-05-17-ad916df843e8.img $OAK/data/ds000054/0.0.1/ $OAK/data/ds000054/0.0.1/derivatives/ participant -w work --participant_label sub-100003 --mem-mb 50000 --nthreads 10 --omp-nthreads 8 -vv
