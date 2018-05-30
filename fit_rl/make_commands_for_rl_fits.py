08a8068eaeb664ac5fd50b9020fb9c116a3b0c3f
Zeynep Enkavi <zeynepenkavi@gmail.com>
2018-05-30 16:12:28 -0700

    working on rl fits scripts

--- fit_rl/make_commands_for_rl_fits.py
+++ fit_rl/make_commands_for_rl_fits.py
@@ -1,10 +1,12 @@
-read in a csv of models
-for each row in the csv create list of fix and fit vars
-get col names where value is 1 -> fit vars
-get col names where value is fix
-concat in pars form dictionary
-append to list of models
+#read in a csv of models
+#for each row in the csv create list of fix and fit vars
+#get col names where value is 1 -> fit vars
+#get col names where value is fix
+#concat in pars form dictionary
+#append to list of models
 
+import numpy as np
+import pandas as pd
 import os
 
 todo_path = os.environ['TODO_PATH']
@@ -17,6 +19,19 @@ subjects = [ x.split("ProbLearn")[1].split(".")[0] for x in beh_file_names ]
 data_path = todo_path+"/behav_data_tb_organized/machine_game/"
 
 output_path = server_scripts+"fit_rl/.fits"
+
+n_fits = 50
+
+rl_models = pd.read_csv('possible_rl_models.csv')
+
+pars_list = []
+
+#pars = {'alpha_neg':np.nan, 'alpha_pos':np.nan, 'beta':np.nan,  'exp_neg':np.nan, 'exp_pos':np.nan}
+
+for row in rl_models:
+    fit_pars = rl_models.columns[(rl_models == "1").iloc[0]]
+    pars = {}
+    pars_list = pars_list.append(pars)
 
 for pars in pars_list:
     for subject in subjects:
