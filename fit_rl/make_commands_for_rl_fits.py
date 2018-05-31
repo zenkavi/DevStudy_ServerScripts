c4c3c842a8712b78fada4a4c21d1beb5d3284241
Zeynep Enkavi <zeynepenkavi@gmail.com>
2018-05-30 17:27:56 -0700

    task list script

--- fit_rl/make_commands_for_rl_fits.py
+++ fit_rl/make_commands_for_rl_fits.py
@@ -28,14 +28,35 @@ pars_list = []
 
 #pars = {'alpha_neg':np.nan, 'alpha_pos':np.nan, 'beta':np.nan,  'exp_neg':np.nan, 'exp_pos':np.nan}
 
-for row in rl_models:
-    fit_pars = rl_models.columns[(rl_models == "1").iloc[0]]
-    pars = {}
-    pars_list = pars_list.append(pars)
+for i in range(len(rl_models)):
+    
+    #cols where row is nan 
+    #rl_models.columns[rl_models.iloc[0].isnull()]
+    
+    fit_pars = list(rl_models.columns[(rl_models == "1").iloc[i]])
+    pars = dict(zip(fit_pars, len(fit_pars)*[np.nan]))
+    
+    #What to fix these too if they are fixed??
+    #Fix values:
+    #alpha - 0.05
+    #beta - 1
+    #exp - 0.5
+    fix_pars = list(rl_models.columns[(rl_models == "fix").iloc[i]])
+    
+    for par in fix_pars:
+        if 'alpha' in par:
+            pars[par] = 0.05
+        elif 'exp' in par:
+            pars[par] = 0.5
+        elif 'beta' in par:
+            pars[par] = 1
+    
+    
+    pars_list.append(pars)
 
 for pars in pars_list:
     for subject in subjects:
-        command = 'python fit_rl.py ' + subject + ' ' + n_fits + ' ' + data_path + ' ' + output_path + ' ' + pars
+        command = 'python fit_rl.py ' + str(subject) + ' ' + str(n_fits) + ' ' + data_path + ' ' + output_path + ' ' + str(pars)
         print(command)
 
 #Sample command
