95dcf0f63354d7d9a20003c86c3cbc3367d6dbfc
Zeynep Enkavi <zeynepenkavi@gmail.com>
2018-06-01 14:28:43 -0700

    added concat script

---
+++ fit_rl/concat_model_fits.py
@@ -0,0 +1,29 @@
+import glob
+import os
+import pandas as pd
+from string import digits
+import sys
+
+#python concat_mode_fits.py
+#python concat_mode_fits.py LearningParams_Fit_alpha-beta-exp_Fix_
+
+try:
+    model_name = sys.argv[1]
+except NameError:
+    model_name = 'all'
+
+data_dir = os.environ['SERVER_SCRIPTS']+"/fit_rl/.fits/"
+
+#helper function
+remove_digits = str.maketrans('', '', digits)
+
+if model_name == 'all':
+    #remove .csv and numbers and get unique
+    all_models = list(set([s.strip('.csv').translate(remove_digits) for s in os.listdir(data_dir)]))
+else:
+    all_models = list(model_name)
+
+for model in all_models:
+    filenames = glob.glob(data_dir+model+'*')
+    all_fits = pd.concat( [ pd.read_csv(f) for f in filenames ] )
+    all_fits.to_csv(data_dir+model+'All.csv')
