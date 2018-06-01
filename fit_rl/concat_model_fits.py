ebda4c4da5f2e7266287e760ed2c15025ce4c7e9
Zeynep Enkavi <zeynepenkavi@gmail.com>
2018-06-01 14:55:27 -0700

    debug concat script

--- fit_rl/concat_model_fits.py
+++ fit_rl/concat_model_fits.py
@@ -26,4 +26,5 @@ else:
 for model in all_models:
     filenames = glob.glob(data_dir+model+'*')
     all_fits = pd.concat( [ pd.read_csv(f) for f in filenames ] )
+    all_fits.reset_index()
     all_fits.to_csv(data_dir+model+'All.csv')
