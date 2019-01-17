b84cafe13e5f44435e11e2a80273c8b2d0950808
Zeynep Enkavi <zeynepenkavi@gmail.com>
2019-01-17 13:00:52 -0800

    debug

--- check_bold_vols.py
+++ check_bold_vols.py
@@ -8,5 +8,5 @@ path = '/oak/stanford/groups/russpold/data/ds000054/0.0.1/'
 boldfiles = glob.glob('%s/sub-[0-9][0-9][0-9][0-9][0-9][0-9]/func/sub-[0-9][0-9][0-9][0-9][0-9][0-9]_task-machinegame_run-0[0-9]_bold.nii.gz'%(path))
 
 for file in boldfiles:
-    print file
+    print(file)
     print(os.system('fslnvols %s'%(file)))
