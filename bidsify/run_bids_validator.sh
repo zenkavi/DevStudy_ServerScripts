305606a0fe0f97497a7b3b2e4d7784cc13b2a332
Zeynep Enkavi <zeynepenkavi@gmail.com>
2019-01-25 12:48:17 -0800

    update validator

--- bidsify/run_bids_validator.sh
+++ bidsify/run_bids_validator.sh
@@ -1,3 +1,4 @@
 module load system
 export DATA_LOC=/oak/stanford/groups/russpold/data/ds000054/0.0.1
-singularity exec /share/PI/russpold/singularity_images/poldracklab_fmriprep_1.2.5-2018-12-04-2ef6b23ede2a.img bids-validator --verbose $DATA_LOC
+export SERVER_SCRIPTS=/oak/stanford/groups/russpold/users/zenkavi/DevStudy_ServerScripts
+singularity exec /share/PI/russpold/singularity_images/poldracklab_fmriprep_1.2.5-2018-12-04-2ef6b23ede2a.img bids-validator --verbose $DATA_LOC > $SERVER_SCRIPTS/bidsify/validator_out.txt
