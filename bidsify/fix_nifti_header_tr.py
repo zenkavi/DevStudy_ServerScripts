31b5f78b06b7f003aa086df97f95fb0b29ec4540
Zeynep Enkavi <zeynepenkavi@gmail.com>
2019-02-01 21:44:11 -0800

    debug

--- bidsify/fix_nifti_header_tr.py
+++ bidsify/fix_nifti_header_tr.py
@@ -2,19 +2,18 @@
 import glob
 import nibabel as nib
 import os
-from shutil import copyfile
 try:
     data_loc = os.environ['DATA_LOC']
 except KeyError:
     os.system('source /oak/stanford/groups/russpold/users/zenkavi/DevStudy_ServerScripts/setup/dev_study_env.sh')
     data_loc = os.environ['DATA_LOC']
 
-bold_file_paths = glob.glob('%s/sub-*/func/sub-*_task-machinegame_run-*_bold.nii.gz'%(data_loc))
-bold_file_paths.sort()
+bold_files = glob.glob('%s/sub-*/func/sub-*_task-machinegame_run-*_bold.nii.gz'%(data_loc))
+bold_files.sort()
 
-bold_file_names = [os.path.basename(x) for x in bold_file_paths]
+#bold_file_names = [os.path.basename(x) for x in bold_file_paths]
 
-for cur_bold in bold_file_paths:
+for cur_bold in bold_files:
     cur_img = nib.load(cur_bold)
     print('Loaded %s'%(cur_bold))
     if cur_img.header['pixdim'][4] != 2:
@@ -24,5 +23,3 @@ for cur_bold in bold_file_paths:
         nib.save(cur_img, cur_bold)
     except:
         print('Failed saving TR for %s'%(cur_bold))
-
-#Will I have to copy back and forth if I can't figure out permissions??
