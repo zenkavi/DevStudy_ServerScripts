487a60a5d4b927a946b6432ed99ded6e8a18b290
Zeynep Enkavi <zeynepenkavi@gmail.com>
2019-02-01 16:57:43 -0800

    added batch script to copy over dataset

--- bidsify/fix_nifti_header_tr.py
+++ bidsify/fix_nifti_header_tr.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python
-import os
 import glob
 import nibabel as nib
+import os
+from shutil import copyfile
 try:
     data_loc = os.environ['DATA_LOC']
 except KeyError:
     os.system('source /oak/stanford/groups/russpold/users/zenkavi/DevStudy_ServerScripts/setup/dev_study_env.sh')
     data_loc = os.environ['DATA_LOC']
 
-bold_files = glob.glob('%s/sub-*/func/sub-*_task-machinegame_run-*_bold.nii.gz'%(data_loc))
+bold_file_paths = glob.glob('%s/sub-*/func/sub-*_task-machinegame_run-*_bold.nii.gz'%(data_loc))
+bold_file_paths.sort()
 
-for cur_bold in bold_files:
+bold_file_names = [os.path.basename(x) for x in bold_file_paths]
+
+for cur_bold in bold_file_paths:
     cur_img = nib.load(cur_bold)
     print('Loaded %s'%(cur_bold))
     if cur_img.header['pixdim'][4] != 2:
@@ -20,3 +24,5 @@ for cur_bold in bold_files:
         nib.save(cur_img, cur_bold)
     except:
         print('Failed saving TR for %s'%(cur_bold))
+
+#Will I have to copy back and forth if I can't figure out permissions??
