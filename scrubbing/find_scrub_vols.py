0dd1821447e7b855856f81c8d76f6246132bc279
Zeynep Enkavi <zeynepenkavi@gmail.com>
2019-02-04 17:09:10 -0800

    added batch script

--- scrubbing/find_scrub_vols.py
+++ scrubbing/find_scrub_vols.py
@@ -17,7 +17,7 @@ except KeyError:
 fmriprep_counfounds_files = glob.glob('%s/derivatives/fmriprep_1.2.5/fmriprep/sub-*/func/sub-*_task-machinegame_run-*_desc-confounds_regressors.tsv'%(data_loc))
 fmriprep_counfounds_files.sort()
 
-scrub_report = pd.DataFrame()
+scrub_report = pd.DataFrame(columns=['sub_id', 'run', 'pct_scrubbed'])
 
 #get index of vols where fd>0.9
 for cur_confounds in fmriprep_counfounds_files:
@@ -27,5 +27,7 @@ for cur_confounds in fmriprep_counfounds_files:
     out_file_name = os.path.basename(cur_confounds)
     out_file_name = re.sub("desc-confounds_regressors.tsv", "scrub_vols.txt", out_file_name)
     np.savetxt(os.path.join(out_path, out_file_name), scrub_vols.astype(int), fmt='%i', delimiter='\n')
+    scrub_report = scrub_report.append({'sub_id': int(re.findall('\d+', out_file_name)[0]), 'run': int(re.findall('\d+', out_file_name)[1]), 'pct_scrubbed': sum(scrub_vols)/len(scrub_vols)*100}, ignore_index=True)
 
-#create one output report on how many/what percent of volumes per run are scrubbed
+#output report on how many/what percent of volumes per run are scrubbed
+scrub_report.to_csv('/oak/stanford/groups/russpold/data/ds000054/0.0.2/derivatives/fmriprep_1.2.5/fmriprep/scrub_report.csv')
