1a469a692767cd08b5f3421f06235798bf5d852b
Zeynep Enkavi <zeynepenkavi@gmail.com>
2019-02-08 11:17:50 -0800

    added new field for fmap jsons

--- bidsify/fix_fmap_jsons.py
+++ bidsify/fix_fmap_jsons.py
@@ -1,52 +1,35 @@
 #!/usr/bin/env python
-#Usage: python fix_fmap_json.py sub_dir_name
-import os, sys, json
-sys.path = ['',
- '/opt/apps/intel16/python/2.7.11/lib/python2.7/site-packages/setuptools-20.2.2-py2.7.egg',
- '/opt/apps/intel16/python/2.7.11/lib/python2.7/site-packages/pip-8.1.0-py2.7.egg',
- '/opt/apps/intel16/python/2.7.11/lib/python2.7/site-packages/matplotlib-1.5.1-py2.7-linux-x86_64.egg',
- '/opt/apps/intel16/python/2.7.11/lib/python2.7/site-packages/pyparsing-2.1.0-py2.7.egg',
- '/opt/apps/intel16/python/2.7.11/lib/python2.7/site-packages/cycler-0.10.0-py2.7.egg',
- '/opt/apps/intel16/python/2.7.11/lib/python2.7/site-packages/pytz-2015.7-py2.7.egg',
- '/opt/apps/intel16/python/2.7.11/lib/python2.7/site-packages/python_dateutil-2.5.0-py2.7.egg',
- '/opt/apps/intel16/python/2.7.11/lib/python2.7/site-packages',
- '/opt/apps/intel16/python/2.7.11/bin',
- '/opt/apps/intel16/cray_mpich_7_3/python/2.7.11/lib/python2.7/site-packages',
- '/work/01329/poldrack/lonestar/software_lonestar5/anaconda/Anaconda2-4.1.1/lib/python2.7/site-packages',
- '/opt/apps/intel16/python/2.7.11/lib/python27.zip',
- '/opt/apps/intel16/python/2.7.11/lib/python2.7',
- '/opt/apps/intel16/python/2.7.11/lib/python2.7/plat-linux2',
- '/opt/apps/intel16/python/2.7.11/lib/python2.7/lib-tk',
- '/opt/apps/intel16/python/2.7.11/lib/python2.7/lib-old',
- '/opt/apps/intel16/python/2.7.11/lib/python2.7/lib-dynload',
- '/opt/apps/intel16/python/2.7.11/lib/python2.7/site-packages/IPython/extensions',
- '/home1/04127/zenkavi/.ipython']
+from collections import OrderedDict
+import glob
+import json
+import os
+import re
 
-if len(sys.argv) < 1:
-    sys.exit("Usage: fix_fmap_jsons.py sub_dir_name")
+try:
+    data_loc = os.environ['DATA_LOC']
+except KeyError:
+    os.system('source /oak/stanford/groups/russpold/users/zenkavi/DevStudy_ServerScripts/setup/dev_study_env.sh')
+    data_loc = os.environ['DATA_LOC']
 
-fmap_path = sys.argv[1]+'/fmap/' #make sure paths end with '/'
+json_files = glob.glob("%s/sub-*/fmap/*.json"%(data_loc))
+json_files.sort()
 
-json_file_names = [s for s in os.listdir(fmap_path) if ".json" in s]
-
-echo_times = []
-
-for i in json_file_names:
-	with open(fmap_path+i) as data_file:
-	    data = json.load(data_file)
-	    echo_times.append(data['EchoTime'])
-
-echo_times = list(set(echo_times))
-
-echo_times_json = {}
-
-for i in range(len(echo_times)):
-	echo_times_json['EchoTime'+str(i+1)]= echo_times[i]
-
-for i in json_file_names:
-	with open(fmap_path+i) as data_file:
-		data = json.load(data_file)
-		del(data['EchoTime'])
-		data.update(echo_times_json)
-	with open(fmap_path+i, "w") as out_file:
-		json.dump(data, out_file)
+for json_file in json_files:
+    subnum = re.findall('\d+', os.path.basename(json_file))[0]
+    with open(json_file) as json_data:
+        json_dict = json.load(json_data)
+        if 'EchoTime' in json_dict:
+            echo_times = []
+            echo_times = list(set(echo_times))
+            echo_times_dict = {}
+            for i in range(len(echo_times)):
+                echo_times_dict['EchoTime'+str(i+1)]= echo_times[i]
+            del(json_dict['EchoTime'])
+            json_dict.update(echo_times_dict)
+        if 'IntendedFor' not in json_dict:
+            bold_files = glob.glob("%s/sub-%s/func/*_bold.nii.gz"%(data_loc, subnum))
+            bold_files = ['func/'+os.path.basename(x) for x in bold_files]
+            json_dict.update({'IntendedFor': bold_files})
+            json_dict = OrderedDict(sorted(json_dict.items()))
+    with open(json_file, "w") as out_file:
+		json.dump(json_dict, out_file, indent=2)
