aad3a07c47b1ab0e4b8802f7852db7e811af5925
Zeynep Enkavi <zeynepenkavi@gmail.com>
2019-02-08 12:54:40 -0800

    updating fmriprep

--- fmriprep/make_commands_for_fmriprep.py
+++ fmriprep/make_commands_for_fmriprep.py
@@ -7,7 +7,5 @@ data_loc = os.environ['DATA_LOC']
 sub_dirnames = [f for f in os.listdir(data_loc) if re.match(r'sub', f)]
 
 for i in sub_dirnames:
-	command = 'singularity run /share/PI/russpold/singularity_images/poldracklab_fmriprep_1.2.5-2018-12-04-2ef6b23ede2a.img $DATA_LOC/ $LOCAL_SCRATCH/out/ participant -w $LOCAL_SCRATCH/work --participant_label ' + i + ' --mem-mb 50000 --nthreads 10 --omp-nthreads 8 -vv'
+	command = 'singularity exec -B ${DATA_LOC}:${DATA_LOC} /share/PI/russpold/singularity_images/poldracklab_fmriprep_1.3.0-1-2019-02-08-1b5ad521ee2a.simg  $DATA_LOC $DATA_LOC/derivatives/fmriprep_1.3.0 participant -w /tmp/work --participant_label ' + i + ' --mem-mb 50000 --nthreads 10 --omp-nthreads 8 -vv --skip_bids_validation'
 	print(command)
-
-#singularity run /share/PI/russpold/singularity_images/poldracklab_fmriprep_1.2.5-2018-12-04-2ef6b23ede2a.img $OAK/data/ds000054/0.0.1/ $OAK/data/ds000054/0.0.1/derivatives/ participant -w work --participant_label sub-100003 --mem-mb 50000 --nthreads 10 --omp-nthreads 8 -vv
--- fmriprep/tmp_lsmove_task_list.sh
+++
@@ -1,5 +0,0 @@
-rsync -r $DATA_LOC/sub-100009 /tmp/data/
-rsync -r $DATA_LOC/sub-100063 /tmp/data/
-rsync -r $DATA_LOC/sub-100207 /tmp/data/
-rsync -r $DATA_LOC/sub-200162 /tmp/data/
-rsync -r $DATA_LOC/sub-200164 /tmp/data/
