249d7e34962bf5060341104783501633003f1e4d
Zeynep Enkavi <zeynepenkavi@gmail.com>
2019-02-10 10:34:23 -0800

    debug and update pipeline notes

--- mriqc/make_commands_for_mriqc.py
+++ mriqc/make_commands_for_mriqc.py
@@ -7,7 +7,5 @@ data_loc = os.environ['DATA_LOC']
 sub_dirnames = [f for f in os.listdir(data_loc) if re.match(r'sub', f)]
 
 for i in sub_dirnames:
-	command = 'singularity run /share/PI/russpold/singularity_images/poldracklab_mriqc_0.14.2-2018-08-21-070e53b20a43.img $DATA_LOC/ $DATA_LOC/derivatives/mriqc_0.14.2/ participant -w $LOCAL_SCRATCH/work --participant_label ' + i + ' --mem_gb 50 --n_procs 10 --ants-nthreads 8 -vvv'
+	command = 'singularity exec -B ${DATA_LOC}:${DATA_LOC} /share/PI/russpold/singularity_images/poldracklab_mriqc_0.14.2-2018-08-21-070e53b20a43.img mriqc $DATA_LOC $DATA_LOC/derivative/mriqc_0.14.2/ participant -w $LOCAL_SCRATCH/work --participant_label ' + i + ' --mem_gb 50 --n_procs 10 --ants-nthreads 8 -vvv'
 	print(command)
-
-#ingularity run /share/PI/russpold/singularity_images/poldracklab_mriqc_0.10.5-2018-05-28-9954e8c774c2.img $DATA_LOC/ $DATA_LOC/derivatives/mriqc_0.10.5/ participant -w $LOCAL_SCRATCH/work --participant_label sub-100003 --mem-gb 50 --n_procs 10 --ants-nthreads 8 -vvv
