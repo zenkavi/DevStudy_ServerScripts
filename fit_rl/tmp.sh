b53a6a67c5eb1423c33fd1132f8311d09489d200
Zeynep Enkavi <zeynepenkavi@gmail.com>
2019-02-11 11:01:37 -0800

    debug

--- fit_rl/tmp.sh
+++ fit_rl/tmp.sh
@@ -1,5 +1,5 @@
 set -e
-for task_list in  fit_beta-exp_neg_fix_alpha-exp_pos_task_list.sh fit_alpha-beta_fix_exp_task_list.sh
+for task_list in  fit_alpha_neg-alpha_pos-beta_fix_exp_task_list.sh fit_alpha-beta_fix_exp_task_list.sh
 do
 sed -e "s/{TASK_LIST}/$task_list/g" fit_rl.batch | sbatch
 done
