fc8d597e095254de40b03580e0e085c1e638fa91
Zeynep Enkavi <zeynepenkavi@gmail.com>
2019-02-15 11:26:39 -0800

    reverting header back to correct tr

--- bidsify/fix_nifti_header_tr.py
+++ bidsify/fix_nifti_header_tr.py
@@ -9,9 +9,10 @@ except KeyError:
     data_loc = os.environ['DATA_LOC']
 
 # Raw data
-# bold_files = glob.glob('%s/sub-*/func/sub-*_task-machinegame_run-*_bold.nii.gz'%(data_loc))
+raw_bold_files = glob.glob('%s/sub-*/func/sub-*_task-machinegame_run-*_bold.nii.gz'%(data_loc))
 # Preprocessed data
 bold_files = glob.glob("%s/derivatives/fmriprep_1.2.5/fmriprep/sub-*/func/sub-*_task-machinegame_run-*_space-MNI152NLin2009cAsym_desc-preproc_bold.nii.gz"%(data_loc))
+bold_files = raw_bold_files.append(bold_files)
 bold_files.sort()
 
 #bold_file_names = [os.path.basename(x) for x in bold_file_paths]
