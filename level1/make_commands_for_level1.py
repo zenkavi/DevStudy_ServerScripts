add0d031c443f894381848f69936c1339dd0652f
Zeynep Enkavi <zeynepenkavi@gmail.com>
2019-02-18 11:03:02 -0800

    debug

--- level1/make_commands_for_level1.py
+++ level1/make_commands_for_level1.py
@@ -3,7 +3,7 @@ import os
 import glob
 import re
 
-# Usage: python make_l1_commands.py > level1_task_list.sh
+# Usage: python make_commands_for_level1.py > level1_task_list.sh
 
 try:
     data_loc = os.environ['DATA_LOC']
@@ -14,11 +14,8 @@ except KeyError:
 subdirs = glob.glob("%s/derivatives/fmriprep_1.3.0/fmriprep/sub-*/func/sub-*_task-machinegame_run-*_space-MNI152NLin2009cAsym_desc-preproc_bold.nii.gz"%(data_loc))
 
 for dir in subdirs:
-
-    subnum = int(re.findall('\d+', os.path.basename(dir))[0])
-    runnum = int(re.findall('\d+', os.path.basename(dir))[1])
-
-	command = 'feat $DATA_LOC/derivatives/level_1/sub-%s/model/sub%s_run-%s_l1.fsf'%(subnum, subnum, runnum)
-	print(command)
-
+    subnum = re.findall('\d+', os.path.basename(dir))[0]
+    runnum = re.findall('\d+', os.path.basename(dir))[1]
+    command = 'feat $DATA_LOC/derivatives/level_1/sub-%s/model/sub-%s_run-%s_l1.fsf'%(subnum, subnum, runnum)
+    print(command)
 #Example command: feat $DATA_LOC/derivatives/level1_inputs/sub-100003/design_sub-100003_run-01.fsf
