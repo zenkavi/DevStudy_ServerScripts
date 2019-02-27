d93fc267bd98ecd7ef04cdeac3443e2e70fabf79
Zeynep Enkavi <zeynepenkavi@gmail.com>
2019-02-26 21:55:33 -0800

    debug

--- bidsify/heudiconv/run_heudiconv.sh
+++ bidsify/heudiconv/run_heudiconv.sh
@@ -1,5 +1,6 @@
+set -e
 jobnum=0
 while read task; do
-  jobnum=$[$jobnum +1]
-  sed "s/{TASK}/$task/g" "s/{JOBNUM}/$jobnum/g" run_heudiconv.batch | sbatch
+  ((jobnum++))
+  sed -e "s/{TASK}/$task/g" -e "s/{JOBNUM}/$jobnum/g" run_heudiconv.batch | sbatch
 done <heudiconv_tasklist.txt
