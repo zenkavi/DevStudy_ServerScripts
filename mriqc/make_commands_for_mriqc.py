ba72ce3ecc200f60d854d501566288fc8be0760b
Zeynep Enkavi <zeynepenkavi@gmail.com>
2019-03-02 13:31:40 -0600

    corrected typo

--- mriqc/make_commands_for_mriqc.py
+++ mriqc/make_commands_for_mriqc.py
@@ -7,5 +7,5 @@ data_loc = os.environ['DATA_LOC']
 sub_dirnames = [f for f in os.listdir(data_loc) if re.match(r'sub', f)]
 
 for i in sub_dirnames:
-	command = 'singularity exec -B ${DATA_LOC}:${DATA_LOC} /share/PI/russpold/singularity_images/poldracklab_mriqc_0.14.2-2018-08-21-070e53b20a43.img mriqc $DATA_LOC $DATA_LOC/derivative/mriqc_0.14.2/ participant -w $LOCAL_SCRATCH/work --participant_label ' + i + ' --mem_gb 50 --n_procs 10 --ants-nthreads 8 -vvv'
+	command = 'singularity exec -B ${DATA_LOC}:${DATA_LOC} /share/PI/russpold/singularity_images/poldracklab_mriqc_0.14.2-2018-08-21-070e53b20a43.img mriqc $DATA_LOC $DATA_LOC/derivatives/mriqc_0.14.2/ participant -w $LOCAL_SCRATCH/work --participant_label ' + i + ' --mem_gb 50 --n_procs 10 --ants-nthreads 8 -vvv'
 	print(command)
