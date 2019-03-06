a173d0a484084c68b2a57a65c258ffdb8b3efaa8
Zeynep Enkavi <zeynepenkavi@gmail.com>
2019-03-06 13:21:23 -0800

    debug

--- level_1/find_scrub_vols.py
+++ level_1/find_scrub_vols.py
@@ -18,14 +18,14 @@ except KeyError:
     os.system('source /oak/stanford/groups/russpold/users/zenkavi/DevStudy_ServerScripts/setup/dev_study_env.sh')
     data_loc = os.environ['DATA_LOC']
 
-#load fmriprep_counfounds file
-fmriprep_counfounds_files = glob.glob('%s/derivatives/fmriprep_1.3.0/fmriprep/sub-*/func/sub-*_task-machinegame_run-*_desc-confounds_regressors.tsv'%(data_loc))
-fmriprep_counfounds_files.sort()
+#load fmriprep_confounds file
+fmriprep_confounds_files = glob.glob('%s/derivatives/fmriprep_1.3.0/fmriprep/sub-*/func/sub-*_task-machinegame_run-*_desc-confounds_regressors.tsv'%(data_loc))
+fmriprep_confounds_files.sort()
 
 scrub_report = pd.DataFrame(columns=['sub_id', 'run', 'pct_scrubbed'])
 
 #get index of vols where fd>0.9
-for cur_confounds in fmriprep_counfounds_files:
+for cur_confounds in fmriprep_confounds_files:
     cur_df = pd.read_csv(cur_confounds, sep='\t')
     scrub_vols = np.where(cur_df.framewise_displacement>fd_thresh,1,0)
     out_file_name = os.path.basename(cur_confounds)
@@ -33,7 +33,7 @@ for cur_confounds in fmriprep_counfounds_files:
     nums = re.findall('\d+', out_file_name)
     subnum = int(nums[0])
     runnum = int(nums[1])
-    out_path = os.path.join(data_loc, 'derivatives/level_1/sub-%s'%(str(subnum)))
+    out_path = os.path.join(data_loc, 'derivatives/level_1/sub-%s'%(subnum))
     os.makedirs(out_path, exist_ok=True)
     np.savetxt(os.path.join(out_path, out_file_name), scrub_vols.astype(int), fmt='%i', delimiter='\n')
     scrub_report = scrub_report.append({'sub_id': subnum, 'run': runnum, 'pct_scrubbed': sum(scrub_vols)/len(scrub_vols)*100}, ignore_index=True)
--- level_1/tmp.sh
+++ level_1/tmp.sh
@@ -1,7 +1,6 @@
 feat $DATA_LOC/derivatives/level_1/sub-100051/model/sub-100051_run-004_l1.fsf
 feat $DATA_LOC/derivatives/level_1/sub-100051/model/sub-100051_run-005_l1.fsf
 feat $DATA_LOC/derivatives/level_1/sub-100051/model/sub-100051_run-003_l1.fsf
-feat $DATA_LOC/derivatives/level_1/sub-100051/model/sub-100051_run-001_l1.fsf
 feat $DATA_LOC/derivatives/level_1/sub-100051/model/sub-100051_run-006_l1.fsf
 feat $DATA_LOC/derivatives/level_1/sub-100051/model/sub-100051_run-002_l1.fsf
 feat $DATA_LOC/derivatives/level_1/sub-200056/model/sub-200056_run-004_l1.fsf
