ba370125e94af5c75bd5528ddc9d025855f6a2b9
Zeynep Enkavi <zeynepenkavi@gmail.com>
2019-03-08 08:21:11 -0800

    working on level 2

--- level_2/make_l2_fsfs.py
+++ level_2/make_l2_fsfs.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 import os
 import glob
-import nibabel as nib
 import re
 
 try:
@@ -13,26 +12,29 @@ except KeyError:
     server_scripts = os.environ['SERVER_SCRIPTS']
 
 fsfdir="%s/derivatives/level_2"%(data_loc)
-subdirs=glob.glob("%s/derivatives/level_1/sub-*/model/run-*.feat"%(data_loc))
+subdirs=glob.glob("%s/derivatives/level_1/sub-*/"%(data_loc))
 subdirs.sort()
 
 for dir in subdirs:
-  subnum = re.findall('\d+', os.path.dirname(dir))[1]
+  subnum = re.findall('\d+', os.path.dirname(dir))[5]
 
   outdir = '"%s/sub-%s/model/sub-%s"'%(fsfdir, subnum, subnum)
-  if not os.path.exists(os.path.dirname(outdir)):
+  if not os.path.exists('%s/sub-%s/model/'%(fsfdir, subnum)):
       os.makedirs(os.path.dirname(outdir))
 
-  ntpts =
+  ntpts = ...
 
-  replacements = {"OUTDIR": outdir, "NTPTS": ntpts}
+
   #OUTDIR: "/oak/stanford/groups/russpold/data/ds000054/0.0.4/derivatives/level_2"
   #FEATDIR1: "/oak/stanford/groups/russpold/data/ds000054/0.0.4/derivatives/level_1/sub-100003/model/run-001.feat"
 
-
-
   if ntpts == 6:
+      replacements = {"OUTDIR": outdir, "NTPTS": ntpts, "FEATDIR1": feadirs[0], "FEATDIR2": feadirs[1], "FEATDIR3": feadirs[2], "FEATDIR4": feadirs[3], "FEATDIR5": feadirs[4], "FEATDIR6": feadirs[5]}
       with open("%s/level_2/template_l2.fsf"%(server_scripts)) as infile:
+  elif ntpts == 5:
+      with open("%s/level_2/template_l2_r5.fsf"%(server_scripts)) as infile:
+  elif ntpts == 3:
+      with open("%s/level_2/template_l2_r3.fsf"%(server_scripts)) as infile:
           with open("%s/sub-%s/model/sub-%s_l2.fsf"%(fsfdir, subnum, subnum), 'w') as outfile:
               for line in infile:
                   for src, target in replacements.items():
