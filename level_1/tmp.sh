ac7e37c0e9ecd177625d949e323ea6c10b65371e
Zeynep Enkavi <zeynepenkavi@gmail.com>
2019-03-08 09:51:47 -0800

    debug and prep l2 batch script

--- level_1/tmp.sh
+++
@@ -1,7 +0,0 @@
-feat $DATA_LOC/derivatives/level_1/sub-100191/model/sub-100191_run-001_l1.fsf
-feat $DATA_LOC/derivatives/level_1/sub-100191/model/sub-100191_run-002_l1.fsf
-feat $DATA_LOC/derivatives/level_1/sub-100191/model/sub-100191_run-003_l1.fsf
-feat $DATA_LOC/derivatives/level_1/sub-100191/model/sub-100191_run-004_l1.fsf
-feat $DATA_LOC/derivatives/level_1/sub-100191/model/sub-100191_run-005_l1.fsf
-feat $DATA_LOC/derivatives/level_1/sub-100191/model/sub-100191_run-006_l1.fsf
-feat $DATA_LOC/derivatives/level_1/sub-100244/model/sub-100244_run-006_l1.fsf
