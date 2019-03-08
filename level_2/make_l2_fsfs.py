bbc8310aee7cab8f32a0d7337951363445c43b0b
Zeynep Enkavi <zeynepenkavi@gmail.com>
2019-03-08 09:28:47 -0800

    working on level 2

--- level_2/make_l2_fsfs.py
+++ level_2/make_l2_fsfs.py
@@ -27,13 +27,23 @@ for dir in subdirs:
   featdirs.sort()
   ntpts = len(featdirs)
   if ntpts == 6:
-      replacements = {"OUTDIR": outdir, "NTPTS": ntpts, "FEATDIR1": '"%s"'%(featdirs[0]), "FEATDIR2": '"%s"'%(featdirs[1]), "FEATDIR3":'"%s"'%(featdirs[2]), "FEATDIR4": '"%s"'%(featdirs[3]), "FEATDIR5": '"%s"'%(featdirs[4]), "FEATDIR6": '"%s"'%(featdirs[5])}
+      replacements = {"OUTDIR": outdir, "NTPTS": str(ntpts), "FEATDIR1": '"%s"'%(featdirs[0]), "FEATDIR2": '"%s"'%(featdirs[1]), "FEATDIR3":'"%s"'%(featdirs[2]), "FEATDIR4": '"%s"'%(featdirs[3]), "FEATDIR5": '"%s"'%(featdirs[4]), "FEATDIR6": '"%s"'%(featdirs[5])}
       with open("%s/level_2/template_l2.fsf"%(server_scripts)) as infile:
+          with open("%s/sub-%s/model/sub-%s_l2.fsf"%(l2dir, subnum, subnum), 'w') as outfile:
+              for line in infile:
+                  for src, target in replacements.items():
+                      line = line.replace(src, target)
+                  outfile.write(line)
   elif ntpts == 5:
-      replacements = {"OUTDIR": outdir, "NTPTS": ntpts, "FEATDIR1": '"%s"'%(featdirs[0]), "FEATDIR2": '"%s"'%(featdirs[1]), "FEATDIR3":'"%s"'%(featdirs[2]), "FEATDIR4": '"%s"'%(featdirs[3]), "FEATDIR5": '"%s"'%(featdirs[4])}
+      replacements = {"OUTDIR": outdir, "NTPTS": str(ntpts), "FEATDIR1": '"%s"'%(featdirs[0]), "FEATDIR2": '"%s"'%(featdirs[1]), "FEATDIR3":'"%s"'%(featdirs[2]), "FEATDIR4": '"%s"'%(featdirs[3]), "FEATDIR5": '"%s"'%(featdirs[4])}
       with open("%s/level_2/template_l2_r5.fsf"%(server_scripts)) as infile:
+          with open("%s/sub-%s/model/sub-%s_l2.fsf"%(l2dir, subnum, subnum), 'w') as outfile:
+              for line in infile:
+                  for src, target in replacements.items():
+                      line = line.replace(src, target)
+                  outfile.write(line)
   elif ntpts == 3:
-      replacements = {"OUTDIR": outdir, "NTPTS": ntpts, "FEATDIR1": '"%s"'%(featdirs[0]), "FEATDIR2": '"%s"'%(featdirs[1]), "FEATDIR3":'"%s"'%(featdirs[2])}
+      replacements = {"OUTDIR": outdir, "NTPTS": str(ntpts), "FEATDIR1": '"%s"'%(featdirs[0]), "FEATDIR2": '"%s"'%(featdirs[1]), "FEATDIR3":'"%s"'%(featdirs[2])}
       with open("%s/level_2/template_l2_r3.fsf"%(server_scripts)) as infile:
           with open("%s/sub-%s/model/sub-%s_l2.fsf"%(l2dir, subnum, subnum), 'w') as outfile:
               for line in infile:
