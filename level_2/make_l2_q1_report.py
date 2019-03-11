6a550cab44f0e2e7297038a03f7dfe56ed3ec33f
Zeynep Enkavi <zeynepenkavi@gmail.com>
2019-03-11 10:05:52 -0700

    change path

--- level_2/make_l2_q1_report.py
+++ level_2/make_l2_q1_report.py
@@ -3,20 +3,20 @@ import glob
 
 try:
     data_loc = os.environ['DATA_LOC']
-    server_scripts = os.environ['SERVER_SCRIPTS']
 except KeyError:
     os.system('source /oak/stanford/groups/russpold/users/zenkavi/DevStudy_ServerScripts/setup/dev_study_env.sh')
     data_loc = os.environ['DATA_LOC']
-    server_scripts = os.environ['SERVER_SCRIPTS']
 
 outfile = "%s/derivatives/level_2/l2_qa.html"%(data_loc)
 
-all_feats = "%s/derivatives/level_2/*/model/*.gfeat"
+all_feats = glob.glob("%s/derivatives/level_2/*/model/*.gfeat"%(data_loc))
 all_feats.sort()
 
 f = open(outfile, "w")
 for file in all_feats:
     f.write("<p>==============================================")
     f.write("<p>%s"%(file))
-    f.write("<IMG SRC=\"%s/inputreg/maskunique_overlay.png\">"%(file))
+    f.write("<IMG SRC=\"https://login.sherlock.stanford.edu/pun/sys/files/fs/%s/inputreg/maskunique_overlay.png\">"%(file))
 f.close()
+
+https://login.sherlock.stanford.edu/pun/sys/files/fs/oak/stanford/groups/russpold/data/ds000054/0.0.4/derivatives/level_2/sub-100003/model/100003.gfeat/inputreg/
