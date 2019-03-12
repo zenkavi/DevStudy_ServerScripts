243b9281b4754bb18e34504418e147e21e428463
Zeynep Enkavi <zeynepenkavi@gmail.com>
2019-03-12 10:45:51 -0700

    debug

---
+++ level_1/tmp.sh
@@ -0,0 +1,4 @@
+feat $DATA_LOC/derivatives/level_1/sub-100243/model/sub-100243_run-003_l1.fsf
+feat $DATA_LOC/derivatives/level_1/sub-100243/model/sub-100243_run-005_l1.fsf
+feat $DATA_LOC/derivatives/level_1/sub-100243/model/sub-100243_run-004_l1.fsf
+feat $DATA_LOC/derivatives/level_1/sub-100243/model/sub-100243_run-006_l1.fsf
--- level_2/tmp.sh
+++ level_2/tmp.sh
@@ -2,9 +2,7 @@ feat $DATA_LOC/derivatives/level_2/sub-100051/model/sub-100051_l2.fsf
 feat $DATA_LOC/derivatives/level_2/sub-100068/model/sub-100068_l2.fsf
 feat $DATA_LOC/derivatives/level_2/sub-200025/model/sub-200025_l2.fsf
 feat $DATA_LOC/derivatives/level_2/sub-409850/model/sub-409850_l2.fsf
-feat $DATA_LOC/derivatives/level_2/sub-100103/model/sub-100103_l2.fsf
 feat $DATA_LOC/derivatives/level_2/sub-100110/model/sub-100110_l2.fsf
-feat $DATA_LOC/derivatives/level_2/sub-100128/model/sub-100128_l2.fsf
 feat $DATA_LOC/derivatives/level_2/sub-100152/model/sub-100152_l2.fsf
 feat $DATA_LOC/derivatives/level_2/sub-100188/model/sub-100188_l2.fsf
 feat $DATA_LOC/derivatives/level_2/sub-100241/model/sub-100241_l2.fsf
