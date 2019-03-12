1b448a49455de42a7d551eb2100ad96ad03e3ceb
Zeynep Enkavi <zeynepenkavi@gmail.com>
2019-03-12 11:37:46 -0700

    debug

--- level_1/tmp4.sh
+++ level_1/tmp4.sh
@@ -1,6 +1,6 @@
-feat $DATA_LOC/derivatives/level_1/sub-200133/model/sub-200133_run-003_l1.fsf
-feat $DATA_LOC/derivatives/level_1/sub-200133/model/sub-200133_run-005_l1.fsf
-feat $DATA_LOC/derivatives/level_1/sub-200133/model/sub-200133_run-004_l1.fsf
-feat $DATA_LOC/derivatives/level_1/sub-200133/model/sub-200133_run-006_l1.fsf
-feat $DATA_LOC/derivatives/level_1/sub-200133/model/sub-200133_run-001_l1.fsf
-feat $DATA_LOC/derivatives/level_1/sub-200133/model/sub-200133_run-002_l1.fsf
+feat $DATA_LOC/derivatives/level_1/sub-200166/model/sub-200166_run-003_l1.fsf
+feat $DATA_LOC/derivatives/level_1/sub-200166/model/sub-200166_run-005_l1.fsf
+feat $DATA_LOC/derivatives/level_1/sub-200166/model/sub-200166_run-004_l1.fsf
+feat $DATA_LOC/derivatives/level_1/sub-200166/model/sub-200166_run-006_l1.fsf
+feat $DATA_LOC/derivatives/level_1/sub-200166/model/sub-200166_run-001_l1.fsf
+feat $DATA_LOC/derivatives/level_1/sub-200166/model/sub-200166_run-002_l1.fsf
