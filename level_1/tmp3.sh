656e2fc901a81b65de37d63af830729e192f5c67
Zeynep Enkavi <zeynepenkavi@gmail.com>
2019-03-11 19:25:43 -0700

    debug

---
+++ level_1/tmp3.sh
@@ -0,0 +1,4 @@
+feat $DATA_LOC/derivatives/level_1/sub-100152/model/sub-100152_run-004_l1.fsf
+feat $DATA_LOC/derivatives/level_1/sub-100152/model/sub-100152_run-005_l1.fsf
+feat $DATA_LOC/derivatives/level_1/sub-100152/model/sub-100152_run-006_l1.fsf
+feat $DATA_LOC/derivatives/level_1/sub-100152/model/sub-100152_run-003_l1.fsf
