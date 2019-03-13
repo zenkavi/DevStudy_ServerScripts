a4ba6139014e2ff385ea9e9035e2eae4bc072009
Zeynep Enkavi <zeynepenkavi@gmail.com>
2019-03-12 22:02:33 -0700

    debug

---
+++ level_1/tmp200025.sh
@@ -0,0 +1,5 @@
+feat $DATA_LOC/derivatives/level_1/sub-200025/model/sub-200025_run-001_l1.fsf
+feat $DATA_LOC/derivatives/level_1/sub-200025/model/sub-200025_run-002_l1.fsf
+feat $DATA_LOC/derivatives/level_1/sub-200025/model/sub-200025_run-003_l1.fsf
+feat $DATA_LOC/derivatives/level_1/sub-200025/model/sub-200025_run-004_l1.fsf
+feat $DATA_LOC/derivatives/level_1/sub-200025/model/sub-200025_run-005_l1.fsf
