3d06f893e8e0891f939d51e6af36c9865d96f0b7
Zeynep Enkavi <zeynepenkavi@gmail.com>
2019-03-13 10:54:56 -0700

    starting to work on group levels

---
+++ level_3/make_l3_fsfs.py
@@ -0,0 +1,3 @@
+OUTPUT_DIR = '"/oak/stanford/groups/russpold/data/ds000054/0.0.4/derivatives/group_level/cope13_EV/model1"'
+FEATDIR1 = '"/oak/stanford/groups/russpold/data/ds000054/0.0.4/derivatives/level_2/sub-100003/model/100003.gfeat/cope13.feat/stats/cope1.nii.gz"'
+featdirs - FEATDIR1 - FEATDIR74
