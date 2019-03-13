b75cb1a9672054d3585b448897c9169de099385f
Zeynep Enkavi <zeynepenkavi@gmail.com>
2019-03-12 21:36:11 -0700

    debug

--- level_1/tmp7.sh
+++
@@ -1,3 +0,0 @@
-feat $DATA_LOC/derivatives/level_1/sub-310949/model/sub-310949_run-001_l1.fsf
-feat $DATA_LOC/derivatives/level_1/sub-310949/model/sub-310949_run-002_l1.fsf
-feat $DATA_LOC/derivatives/level_1/sub-310949/model/sub-310949_run-003_l1.fsf
