c623f4ab010389baf7e785403dde62a8515dfd89
Zeynep Enkavi <zeynepenkavi@gmail.com>
2019-03-13 08:53:09 -0700

    cleanup

--- level_1/tmp100051.sh
+++
@@ -1,5 +0,0 @@
-feat $DATA_LOC/derivatives/level_1/sub-100051/model/sub-100051_run-002_l1.fsf
-feat $DATA_LOC/derivatives/level_1/sub-100051/model/sub-100051_run-003_l1.fsf
-feat $DATA_LOC/derivatives/level_1/sub-100051/model/sub-100051_run-004_l1.fsf
-feat $DATA_LOC/derivatives/level_1/sub-100051/model/sub-100051_run-005_l1.fsf
-feat $DATA_LOC/derivatives/level_1/sub-100051/model/sub-100051_run-006_l1.fsf
--- level_1/tmp408988.sh
+++
@@ -1,1 +0,0 @@
-feat $DATA_LOC/derivatives/level_1/sub-408988/model/sub-408988_run-005_l1.fsf
