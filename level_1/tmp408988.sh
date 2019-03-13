aed5ce6e2d16b9fbc585ed0da26795649f4277bb
Zeynep Enkavi <zeynepenkavi@gmail.com>
2019-03-12 21:26:06 -0700

    debug

---
+++ level_1/tmp408988.sh
@@ -0,0 +1,6 @@
+feat $DATA_LOC/derivatives/level_1/sub-408988/model/sub-408988_run-001_l1.fsf
+feat $DATA_LOC/derivatives/level_1/sub-408988/model/sub-408988_run-002_l1.fsf
+feat $DATA_LOC/derivatives/level_1/sub-408988/model/sub-408988_run-003_l1.fsf
+feat $DATA_LOC/derivatives/level_1/sub-408988/model/sub-408988_run-004_l1.fsf
+feat $DATA_LOC/derivatives/level_1/sub-408988/model/sub-408988_run-005_l1.fsf
+feat $DATA_LOC/derivatives/level_1/sub-408988/model/sub-408988_run-006_l1.fsf
