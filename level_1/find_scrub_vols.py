be8ccc5018c4baee007ee8a3a9d05bc8c0d59cb0
Zeynep Enkavi <zeynepenkavi@gmail.com>
2019-03-14 17:59:21 -0700

    updated scrub vols to exclude runs that are more than 20 pct scrubbed

--- level_1/find_scrub_vols.py
+++ level_1/find_scrub_vols.py
@@ -5,12 +5,15 @@ import os
 import pandas as pd
 import re
 import sys
+from argparse import ArgumentParser
 
-#Usage: find_scrub_vols.py fd_thresh
-#Acceptable fd_thresh = 0.5
-#Stringent fd_thresh = 0.2
+parser = ArgumentParser()
+parser.add_argument("-fd", "--fd_thresh", default=0.5, help="FD threshold to scrub")
+parser.add_argument("-ep", "--exclude_percent", default=20 , help="Threshold of percentage of scrubbed volumes for excluding a run")
+args = parser.parse_args()
 
-fd_thresh = float(sys.argv[1])
+fd_thresh = args.fd_thresh
+exclude_percent = args.exclude_percent
 
 try:
     data_loc = os.environ['DATA_LOC']
@@ -36,7 +39,16 @@ for cur_confounds in fmriprep_confounds_files:
     out_path = os.path.join(data_loc, 'derivatives/level_1/sub-%s'%(subnum))
     os.makedirs(out_path, exist_ok=True)
     np.savetxt(os.path.join(out_path, out_file_name), scrub_vols.astype(int), fmt='%i', delimiter='\n')
-    scrub_report = scrub_report.append({'sub_id': subnum, 'run': runnum, 'pct_scrubbed': sum(scrub_vols)/len(scrub_vols)*100}, ignore_index=True)
+    pct_scrubbed = sum(scrub_vols)/len(scrub_vols)*100
+    scrub_report = scrub_report.append({'sub_id': subnum, 'run': runnum, 'pct_scrubbed': pct_scrubbed}, ignore_index=True)
+
+    #remove that run from derivatives/fmriprep so it is not used for further processing if pct_scrubbed>20%
+    if pct_scrubbed > exclude_percent:
+        mv_path = '%s/sourcedata/derivatives/fmriprep/sub-%s/'%(data_loc,str(subnum))
+        os.makedirs(mv_path, exist_ok=True)
+        mv_files = glob.glob('%s/derivatives/fmriprep_1.3.0/fmriprep/sub-%s/func/*sub-%s*run-00%s*'%(data_loc,str(subnum),str(subnum),str(runnum)))
+        for i in mv_files:
+            os.rename(i, os.path.join(mv_path, os.path.basename(i)))
 
 #output report on how many/what percent of volumes per run are scrubbed
 scrub_report.to_csv('%s/derivatives/level_1/scrub_fd_%s_report.csv'%(data_loc, str(fd_thresh)))
