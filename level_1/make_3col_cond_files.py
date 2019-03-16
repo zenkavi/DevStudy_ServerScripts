a53fdb44522d219ae40e1ed7147adb82ba364e15
Zeynep Enkavi <zeynepenkavi@gmail.com>
2019-03-16 16:45:34 -0700

    debug

--- level_1/make_3col_cond_files.py
+++ level_1/make_3col_cond_files.py
@@ -109,7 +109,7 @@ for cur_ef in events_files:
     cond_pe_hv.PE = cond_pe_hv.PE - cond_pe_hv.PE.mean()
     if len(cond_pe_hv)<1:
         cond_pe_hv = pd.DataFrame(data={'onset': [0], 'duration': [0], 'PE': [0]})[['onset', 'duration', 'PE']]
-    np.savetxt(r'%s%s/sub-%s_task-machinegame_run-%s_pe_hv.txt'%(out_path,subnum,subnum,runnum), cond_pe_lv.values, fmt='%1.3f')
+    np.savetxt(r'%s%s/sub-%s_task-machinegame_run-%s_pe_hv.txt'%(out_path,subnum,subnum,runnum), cond_pe_hv.values, fmt='%1.3f')
 
     df['response_shift'] = df.response.shift(-1)
     cond_junk = df.query('response == 0 & response_shift == 0')[['onset', 'duration']]
