eda378087776ce30cd105ca314951beb36bd394c
Zeynep Enkavi <zeynepenkavi@gmail.com>
2019-03-17 11:27:07 -0700

    debug

--- level_2/tmp.sh
+++ level_2/tmp.sh
@@ -1,3 +1,3 @@
 feat $DATA_LOC/derivatives/level_2/sub-100009/model1/sub-100009_l2.fsf
-feat $DATA_LOC/derivatives/level_2/sub-100009/model1/sub-200056_l2.fsf
-feat $DATA_LOC/derivatives/level_2/sub-100009/model1/sub-408952_l2.fsf
+feat $DATA_LOC/derivatives/level_2/sub-200056/model1/sub-200056_l2.fsf
+feat $DATA_LOC/derivatives/level_2/sub-408952/model1/sub-408952_l2.fsf
