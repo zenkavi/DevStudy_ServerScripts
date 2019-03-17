dc69f916e4d0560dab6a17e9fa1b8b3088f4f9b0
Zeynep Enkavi <zeynepenkavi@gmail.com>
2019-03-17 12:12:26 -0700

    debug

--- level_3/make_l3_fsfs.py
+++ level_3/make_l3_fsfs.py
@@ -1,3 +1,15 @@
+ls /oak/stanford/groups/russpold/data/ds000054/0.0.4/derivatives/level_2/sub*/model1/*.gfeat/cope1.feat/stats/cope1.nii.gz
+ls /oak/stanford/groups/russpold/data/ds000054/0.0.4/derivatives/level_2/sub*/model1/*.gfeat/cope2.feat/stats/cope1.nii.gz
+ls /oak/stanford/groups/russpold/data/ds000054/0.0.4/derivatives/level_2/sub*/model1/*.gfeat/cope3.feat/stats/cope1.nii.gz
+ls /oak/stanford/groups/russpold/data/ds000054/0.0.4/derivatives/level_2/sub*/model1/*.gfeat/cope4.feat/stats/cope1.nii.gz
+ls /oak/stanford/groups/russpold/data/ds000054/0.0.4/derivatives/level_2/sub*/model1/*.gfeat/cope5.feat/stats/cope1.nii.gz
+ls /oak/stanford/groups/russpold/data/ds000054/0.0.4/derivatives/level_2/sub*/model1/*.gfeat/cope6.feat/stats/cope1.nii.gz
+ls /oak/stanford/groups/russpold/data/ds000054/0.0.4/derivatives/level_2/sub*/model1/*.gfeat/cope7.feat/stats/cope1.nii.gz
+ls /oak/stanford/groups/russpold/data/ds000054/0.0.4/derivatives/level_2/sub*/model1/*.gfeat/cope8.feat/stats/cope1.nii.gz
+ls /oak/stanford/groups/russpold/data/ds000054/0.0.4/derivatives/level_2/sub*/model1/*.gfeat/cope9.feat/stats/cope1.nii.gz
+ls /oak/stanford/groups/russpold/data/ds000054/0.0.4/derivatives/level_2/sub*/model1/*.gfeat/cope10.feat/stats/cope1.nii.gz
+ls /oak/stanford/groups/russpold/data/ds000054/0.0.4/derivatives/level_2/sub*/model1/*.gfeat/cope11.feat/stats/cope1.nii.gz
+
 OUTPUT_DIR = '"/oak/stanford/groups/russpold/data/ds000054/0.0.4/derivatives/group_level/cope13_EV/model1"'
 FEATDIR1 = '"/oak/stanford/groups/russpold/data/ds000054/0.0.4/derivatives/level_2/sub-100003/model/100003.gfeat/cope13.feat/stats/cope1.nii.gz"'
 featdirs - FEATDIR1 - FEATDIR74
