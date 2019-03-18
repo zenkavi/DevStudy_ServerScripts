960a5522384c61659235d898564b85d885358bfc
Zeynep Enkavi <zeynepenkavi@gmail.com>
2019-03-18 16:11:27 -0700

    debug

--- rm_dir.sh
+++ rm_dir.sh
@@ -1,2 +1,3 @@
-rsync -a --delete empty_dir/    /oak/stanford/groups/russpold/data/ds000054/0.0.4/derivatives/level_2/
-rsync -a --delete empty_dir/    /oak/stanford/groups/russpold/data/ds000054/0.0.4/derivatives/level_3/
+rsync -a --delete /oak/stanford/groups/russpold/data/ds000054/0.0.4/derivatives/empty_dir/ /oak/stanford/groups/russpold/data/ds000054/0.0.4/derivatives/level_1/
+rsync -a --delete /oak/stanford/groups/russpold/data/ds000054/0.0.4/derivatives/empty_dir/ /oak/stanford/groups/russpold/data/ds000054/0.0.4/derivatives/level_2/
+rsync -a --delete /oak/stanford/groups/russpold/data/ds000054/0.0.4/derivatives/empty_dir/ /oak/stanford/groups/russpold/data/ds000054/0.0.4/derivatives/level_3/
