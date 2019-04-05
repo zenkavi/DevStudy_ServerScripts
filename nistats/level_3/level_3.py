b99a4f6f48d1fa5adf54b4df3086ada9803c7588
Zeynep Enkavi <zeynepenkavi@gmail.com>
2019-04-05 08:56:05 -0700

    debug

--- nistats/level_3/level_3.py
+++ nistats/level_3/level_3.py
@@ -9,12 +9,12 @@ import pickle
 import re
 from argparse import ArgumentParser
 
-#Usage: python level_3.py -m MNUM -r REG -s True
+#Usage: python level_3.py -m MNUM -r REG --runstats
 
 parser = ArgumentParser()
 parser.add_argument("-m", "--mnum", help="model number")
 parser.add_argument("-r", "--reg", help="regressor name")
-parser.add_argument("-s", "--runstats", help="run GLM and save contrasts", action='store_true', default=True)
+parser.add_argument("--runstats", help="run GLM and save contrasts", action='store_true')
 args = parser.parse_args()
 mnum = args.mnum
 reg = args.reg
