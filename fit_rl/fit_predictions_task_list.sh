35aa8bba7bdaf8cecc2fd6d293f0b880621976d5
Zeynep Enkavi <zeynepenkavi@gmail.com>
2019-04-12 17:39:28 -0500

    debug

--- fit_rl/fit_predictions_task_list.sh
+++ fit_rl/fit_predictions_task_list.sh
@@ -1,5 +1,5 @@
 python get_fit_predictions.py --model_name Fit_alpha_neg-alpha_pos-beta-exp_Fix
-python get_fit_predictions.py --model_name Fit_alpha_neg-alpha_pos-beta-exp_neg-exp_pos
+python get_fit_predictions.py --model_name Fit_alpha_neg-alpha_pos-beta-exp_neg-exp_pos_Fix
 python get_fit_predictions.py --model_name Fit_alpha-beta-exp_Fix
 python get_fit_predictions.py --model_name Fit_alpha-beta-exp_neg-exp_pos_Fix
 python get_fit_predictions.py --model_name Fit_alpha-beta_Fix_exp
