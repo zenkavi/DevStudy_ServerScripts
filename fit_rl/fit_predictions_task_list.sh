5434c5955b160445a1e5faa6c6b68ca1a7185062
Zeynep Enkavi <zeynepenkavi@gmail.com>
2019-04-12 15:48:12 -0500

    expand fit predictions

---
+++ fit_rl/fit_predictions_task_list.sh
@@ -0,0 +1,6 @@
+python get_fit_predictions.py --model_name Fit_alpha_neg-alpha_pos-beta-exp_Fix
+python get_fit_predictions.py --model_name Fit_alpha_neg-alpha_pos-beta-exp_neg-exp_pos
+python get_fit_predictions.py --model_name Fit_alpha-beta-exp_Fix
+python get_fit_predictions.py --model_name Fit_alpha-beta-exp_neg-exp_pos_Fix
+python get_fit_predictions.py --model_name Fit_alpha-beta_Fix_exp
+python get_fit_predictions.py --model_name Fit_alpha_neg-alpha_pos-beta_Fix_exp
