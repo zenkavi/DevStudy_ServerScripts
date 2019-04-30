0b1275f93dc77b85951f8af3ff14257ae1e0a5d6
Zeynep Enkavi <zeynepenkavi@gmail.com>
2019-04-29 19:16:35 -0700

    added task lists

--- fit_rl/run_fit_rl.sh
+++ fit_rl/run_fit_rl.sh
@@ -1,5 +1,5 @@
 set -e
-for task_list in fit_alpha_neg-alpha_pos-beta-exp_fix_task_list.sh fit_alpha_neg-alpha_pos-beta-exp_neg-exp_pos_fix_task_list.sh fit_alpha-beta-exp_fix_task_list.sh fit_alpha-beta-exp_neg-exp_pos_fix_task_list.sh fit_alpha-beta_fix_exp_task_list.sh fit_alpha_neg-alpha_pos-beta_fix_exp_task_list.sh
+for task_list in fit_alpha_neg-alpha_pos-beta-exp_neg-exp_pos-lossave_fix_task_list.sh fit_alpha_neg-alpha_pos-beta-exp_fix_lossave_task_list.sh fit_alpha-beta-lossave_fix_exp_task_list.sh fit_alpha_neg-alpha_pos-beta-exp_neg-exp_pos_fix_lossave_task_list.sh fit_alpha-beta-exp-lossave_fix_task_list.sh fit_alpha-beta_fix_exp-lossave_task_list.sh fit_alpha-beta-exp_neg-exp_pos-lossave_fix_task_list.sh fit_alpha-beta-exp_fix_lossave_task_list.sh fit_alpha_neg-alpha_pos-beta-exp-lossave_fix_task_list.sh fit_alpha-beta-exp_neg-exp_pos_fix_lossave_task_list.sh fit_alpha_neg-alpha_pos-beta-lossave_fix_exp_task_list.sh fit_alpha_neg-alpha_pos-beta_fix_exp-lossave_task_list.sh
 do
 sed -e "s/{TASK_LIST}/$task_list/g" fit_rl.batch | sbatch
 done
