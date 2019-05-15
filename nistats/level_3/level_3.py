e029af716c6b752ebeea1816b4d0cbc8bb510a91
Zeynep Enkavi <zeynepenkavi@gmail.com>
2019-05-15 12:38:09 -0700

    updated learners

--- nistats/level_3/level_3.py
+++ nistats/level_3/level_3.py
@@ -78,7 +78,7 @@ if mnum == "model3":
 
 #model4: learners vs non-learners
 if mnum == "model4":
-    design_matrix = age_info[['learner', 'non_learner']]
+    design_matrix = learner_info[['learner', 'non_learner']]
     design_matrix['intercept'] = [1] * len(level2_images)
     if not os.path.exists("%s/rand_anova"%(out_path)):
         os.mkdir("%s/rand_anova"%(out_path))
