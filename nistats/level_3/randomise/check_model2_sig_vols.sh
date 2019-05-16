a54bc4602f2852fe3e3cc6d1d0637dbbd3ee028d
Zeynep Enkavi <zeynepenkavi@gmail.com>
2019-05-15 20:22:59 -0700

    automate checking voxels

---
+++ nistats/level_3/randomise/check_model2_sig_vols.sh
@@ -0,0 +1,24 @@
+echo fslstats model2/m1/rand_anova/group_diff_tfce_corrp_fstat1.nii.gz -l 0.95 -V
+fslstats /oak/stanford/groups/russpold/data/ds000054/0.0.4/derivatives/nistats/level_3/model2/m1/rand_anova/group_diff_tfce_corrp_fstat1.nii.gz -l 0.95 -V
+echo fslstats model2/m2/rand_anova/group_diff_tfce_corrp_fstat1.nii.gz -l 0.95 -V
+fslstats /oak/stanford/groups/russpold/data/ds000054/0.0.4/derivatives/nistats/level_3/model2/m2/rand_anova/group_diff_tfce_corrp_fstat1.nii.gz -l 0.95 -V
+echo fslstats model2/m3/rand_anova/group_diff_tfce_corrp_fstat1.nii.gz -l 0.95 -V
+fslstats /oak/stanford/groups/russpold/data/ds000054/0.0.4/derivatives/nistats/level_3/model2/m3/rand_anova/group_diff_tfce_corrp_fstat1.nii.gz -l 0.95 -V
+echo fslstats model2/m4/rand_anova/group_diff_tfce_corrp_fstat1.nii.gz -l 0.95 -V
+fslstats /oak/stanford/groups/russpold/data/ds000054/0.0.4/derivatives/nistats/level_3/model2/m4/rand_anova/group_diff_tfce_corrp_fstat1.nii.gz -l 0.95 -V
+echo fslstats model2/m1_rt/rand_anova/group_diff_tfce_corrp_fstat1.nii.gz -l 0.95 -V
+fslstats /oak/stanford/groups/russpold/data/ds000054/0.0.4/derivatives/nistats/level_3/model2/m1_rt/rand_anova/group_diff_tfce_corrp_fstat1.nii.gz -l 0.95 -V
+echo fslstats model2/m2_rt/rand_anova/group_diff_tfce_corrp_fstat1.nii.gz -l 0.95 -V
+fslstats /oak/stanford/groups/russpold/data/ds000054/0.0.4/derivatives/nistats/level_3/model2/m2_rt/rand_anova/group_diff_tfce_corrp_fstat1.nii.gz -l 0.95 -V
+echo fslstats model2/m3_rt/rand_anova/group_diff_tfce_corrp_fstat1.nii.gz -l 0.95 -V
+fslstats /oak/stanford/groups/russpold/data/ds000054/0.0.4/derivatives/nistats/level_3/model2/m3_rt/rand_anova/group_diff_tfce_corrp_fstat1.nii.gz -l 0.95 -V
+echo fslstats model2/m4_rt/rand_anova/group_diff_tfce_corrp_fstat1.nii.gz -l 0.95 -V
+fslstats /oak/stanford/groups/russpold/data/ds000054/0.0.4/derivatives/nistats/level_3/model2/m4_rt/rand_anova/group_diff_tfce_corrp_fstat1.nii.gz -l 0.95 -V
+echo fslstats model2/pe/rand_anova/group_diff_tfce_corrp_fstat1.nii.gz -l 0.95 -V
+fslstats /oak/stanford/groups/russpold/data/ds000054/0.0.4/derivatives/nistats/level_3/model2/pe/rand_anova/group_diff_tfce_corrp_fstat1.nii.gz -l 0.95 -V
+echo fslstats model2/junk/rand_anova/group_diff_tfce_corrp_fstat1.nii.gz -l 0.95 -V
+fslstats /oak/stanford/groups/russpold/data/ds000054/0.0.4/derivatives/nistats/level_3/model2/junk/rand_anova/group_diff_tfce_corrp_fstat1.nii.gz -l 0.95 -V
+echo fslstats model2/task_on/rand_anova/group_diff_tfce_corrp_fstat1.nii.gz -l 0.95 -V
+fslstats /oak/stanford/groups/russpold/data/ds000054/0.0.4/derivatives/nistats/level_3/model2/task_on/rand_anova/group_diff_tfce_corrp_fstat1.nii.gz -l 0.95 -V
+echo fslstats model2/rt/rand_anova/group_diff_tfce_corrp_fstat1.nii.gz -l 0.95 -V
+fslstats /oak/stanford/groups/russpold/data/ds000054/0.0.4/derivatives/nistats/level_3/model2/rt/rand_anova/group_diff_tfce_corrp_fstat1.nii.gz -l 0.95 -V
