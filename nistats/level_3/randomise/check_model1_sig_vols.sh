0f7c5d09a9db337c65d77a7d8ed07a21420979ee
Zeynep Enkavi <zeynepenkavi@gmail.com>
2019-05-16 14:17:40 -0700

    add m1 sig checking

---
+++ nistats/level_3/randomise/check_model1_sig_vols.sh
@@ -0,0 +1,24 @@
+
+echo fslstats model1/hpe/rand_anova/group_diff_tfce_corrp_tstat1.nii.gz -l 0.95 -V
+fslstats /oak/stanford/groups/russpold/data/ds000054/0.0.4/derivatives/nistats/level_3/model1/hpe/rand_anova/group_diff_tfce_corrp_tstat1.nii.gz -l 0.95 -V
+
+echo fslstats model1/hpe/rand_anova/group_diff_tfce_corrp_tstat2.nii.gz -l 0.95 -V
+fslstats /oak/stanford/groups/russpold/data/ds000054/0.0.4/derivatives/nistats/level_3/model1/hpe/rand_anova/group_diff_tfce_corrp_tstat2.nii.gz -l 0.95 -V
+
+echo fslstats model1/lpe/rand_anova/group_diff_tfce_corrp_tstat1.nii.gz -l 0.95 -V
+fslstats /oak/stanford/groups/russpold/data/ds000054/0.0.4/derivatives/nistats/level_3/model1/lpe/rand_anova/group_diff_tfce_corrp_tstat1.nii.gz -l 0.95 -V
+
+echo fslstats model1/lpe/rand_anova/group_diff_tfce_corrp_tstat2.nii.gz -l 0.95 -V
+fslstats /oak/stanford/groups/russpold/data/ds000054/0.0.4/derivatives/nistats/level_3/model1/lpe/rand_anova/group_diff_tfce_corrp_tstat2.nii.gz -l 0.95 -V
+
+echo fslstats model1/task_on/rand_anova/group_diff_tfce_corrp_tstat1.nii.gz -l 0.95 -V
+fslstats /oak/stanford/groups/russpold/data/ds000054/0.0.4/derivatives/nistats/level_3/model1/task_on/rand_anova/group_diff_tfce_corrp_tstat1.nii.gz -l 0.95 -V
+
+echo fslstats model1/task_on/rand_anova/group_diff_tfce_corrp_tstat2.nii.gz -l 0.95 -V
+fslstats /oak/stanford/groups/russpold/data/ds000054/0.0.4/derivatives/nistats/level_3/model1/task_on/rand_anova/group_diff_tfce_corrp_tstat2.nii.gz -l 0.95 -V
+
+echo fslstats model1/rt/rand_anova/group_diff_tfce_corrp_tstat1.nii.gz -l 0.95 -V
+fslstats /oak/stanford/groups/russpold/data/ds000054/0.0.4/derivatives/nistats/level_3/model1/rt/rand_anova/group_diff_tfce_corrp_tstat1.nii.gz -l 0.95 -V
+
+echo fslstats model1/rt/rand_anova/group_diff_tfce_corrp_tstat2.nii.gz -l 0.95 -V
+fslstats /oak/stanford/groups/russpold/data/ds000054/0.0.4/derivatives/nistats/level_3/model1/rt/rand_anova/group_diff_tfce_corrp_tstat2.nii.gz -l 0.95 -V
