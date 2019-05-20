66a76b13ad259b3f9179384c76b2b9dcd32d53e1
Zeynep Enkavi <zeynepenkavi@gmail.com>
2019-05-19 19:17:28 -0700

    added variance smoothing for randomise

--- nistats/level_3/randomise/rand_model1_task_list.sh
+++ nistats/level_3/randomise/rand_model1_task_list.sh
@@ -1,15 +1,15 @@
-randomise -i $MODEL_DIR/m1/all_l2_model1_m1.nii.gz -o $MODEL_DIR/m1/rand_baseline/group_diff -d $MODEL_DIR/m1/model1_m1.mat -t $MODEL_DIR/design.con -n 1000 -T
-randomise -i $MODEL_DIR/m2/all_l2_model1_m2.nii.gz -o $MODEL_DIR/m2/rand_baseline/group_diff -d $MODEL_DIR/m2/model1_m2.mat -t $MODEL_DIR/design.con -n 1000 -T
-randomise -i $MODEL_DIR/m3/all_l2_model1_m3.nii.gz -o $MODEL_DIR/m3/rand_baseline/group_diff -d $MODEL_DIR/m3/model1_m3.mat -t $MODEL_DIR/design.con -n 1000 -T
-randomise -i $MODEL_DIR/m4/all_l2_model1_m4.nii.gz -o $MODEL_DIR/m4/rand_baseline/group_diff -d $MODEL_DIR/m4/model1_m4.mat -t $MODEL_DIR/design.con -n 1000 -T
-randomise -i $MODEL_DIR/m1_rt/all_l2_model1_m1_rt.nii.gz -o $MODEL_DIR/m1_rt/rand_baseline/group_diff -d $MODEL_DIR/m1_rt/model1_m1_rt.mat -t $MODEL_DIR/design.con -n 1000 -T
-randomise -i $MODEL_DIR/m2_rt/all_l2_model1_m2_rt.nii.gz -o $MODEL_DIR/m2_rt/rand_baseline/group_diff -d $MODEL_DIR/m2_rt/model1_m2_rt.mat -t $MODEL_DIR/design.con -n 1000 -T
-randomise -i $MODEL_DIR/m3_rt/all_l2_model1_m3_rt.nii.gz -o $MODEL_DIR/m3_rt/rand_baseline/group_diff -d $MODEL_DIR/m3_rt/model1_m3_rt.mat -t $MODEL_DIR/design.con -n 1000 -T
-randomise -i $MODEL_DIR/m4_rt/all_l2_model1_m4_rt.nii.gz -o $MODEL_DIR/m4_rt/rand_baseline/group_diff -d $MODEL_DIR/m4_rt/model1_m4_rt.mat -t $MODEL_DIR/design.con -n 1000 -T
-randomise -i $MODEL_DIR/hpe/all_l2_model1_hpe.nii.gz -o $MODEL_DIR/hpe/rand_baseline/group_diff -d $MODEL_DIR/hpe/model1_hpe.mat -t $MODEL_DIR/design.con -n 1000 -T
-randomise -i $MODEL_DIR/lpe/all_l2_model1_lpe.nii.gz -o $MODEL_DIR/lpe/rand_baseline/group_diff -d $MODEL_DIR/lpe/model1_lpe.mat -t $MODEL_DIR/design.con -n 1000 -T
-randomise -i $MODEL_DIR/junk/all_l2_model1_junk.nii.gz -o $MODEL_DIR/junk/rand_baseline/group_diff -d $MODEL_DIR/junk/model1_junk.mat -t $MODEL_DIR/design.con -n 1000 -T
-randomise -i $MODEL_DIR/task_on/all_l2_model1_task_on.nii.gz -o $MODEL_DIR/task_on/rand_baseline/group_diff -d $MODEL_DIR/task_on/model1_task_on.mat -t $MODEL_DIR/design.con -n 1000 -T
-randomise -i $MODEL_DIR/rt/all_l2_model1_rt.nii.gz -o $MODEL_DIR/rt/rand_baseline/group_diff -d $MODEL_DIR/rt/model1_rt.mat -t $MODEL_DIR/design.con -n 1000 -T
-randomise -i $MODEL_DIR/var_sen/all_l2_model1_var_sen.nii.gz -o $MODEL_DIR/var_sen/rand_baseline/group_diff -d $MODEL_DIR/var_sen/model1_var_sen.mat -t $MODEL_DIR/design.con -n 1000 -T
-randomise -i $MODEL_DIR/ev_sen/all_l2_model1_ev_sen.nii.gz -o $MODEL_DIR/ev_sen/rand_baseline/group_diff -d $MODEL_DIR/ev_sen/model1_ev_sen.mat -t $MODEL_DIR/design.con -n 1000 -T
+randomise -i $MODEL_DIR/m1/all_l2_model1_m1.nii.gz -o $MODEL_DIR/m1/rand_baseline/group_diff -d $MODEL_DIR/m1/model1_m1.mat -t $MODEL_DIR/design.con -n 1000 -T -v 5
+randomise -i $MODEL_DIR/m2/all_l2_model1_m2.nii.gz -o $MODEL_DIR/m2/rand_baseline/group_diff -d $MODEL_DIR/m2/model1_m2.mat -t $MODEL_DIR/design.con -n 1000 -T -v 5
+randomise -i $MODEL_DIR/m3/all_l2_model1_m3.nii.gz -o $MODEL_DIR/m3/rand_baseline/group_diff -d $MODEL_DIR/m3/model1_m3.mat -t $MODEL_DIR/design.con -n 1000 -T -v 5
+randomise -i $MODEL_DIR/m4/all_l2_model1_m4.nii.gz -o $MODEL_DIR/m4/rand_baseline/group_diff -d $MODEL_DIR/m4/model1_m4.mat -t $MODEL_DIR/design.con -n 1000 -T -v 5
+randomise -i $MODEL_DIR/m1_rt/all_l2_model1_m1_rt.nii.gz -o $MODEL_DIR/m1_rt/rand_baseline/group_diff -d $MODEL_DIR/m1_rt/model1_m1_rt.mat -t $MODEL_DIR/design.con -n 1000 -T -v 5
+randomise -i $MODEL_DIR/m2_rt/all_l2_model1_m2_rt.nii.gz -o $MODEL_DIR/m2_rt/rand_baseline/group_diff -d $MODEL_DIR/m2_rt/model1_m2_rt.mat -t $MODEL_DIR/design.con -n 1000 -T -v 5
+randomise -i $MODEL_DIR/m3_rt/all_l2_model1_m3_rt.nii.gz -o $MODEL_DIR/m3_rt/rand_baseline/group_diff -d $MODEL_DIR/m3_rt/model1_m3_rt.mat -t $MODEL_DIR/design.con -n 1000 -T -v 5
+randomise -i $MODEL_DIR/m4_rt/all_l2_model1_m4_rt.nii.gz -o $MODEL_DIR/m4_rt/rand_baseline/group_diff -d $MODEL_DIR/m4_rt/model1_m4_rt.mat -t $MODEL_DIR/design.con -n 1000 -T -v 5
+randomise -i $MODEL_DIR/hpe/all_l2_model1_hpe.nii.gz -o $MODEL_DIR/hpe/rand_baseline/group_diff -d $MODEL_DIR/hpe/model1_hpe.mat -t $MODEL_DIR/design.con -n 1000 -T -v 5
+randomise -i $MODEL_DIR/lpe/all_l2_model1_lpe.nii.gz -o $MODEL_DIR/lpe/rand_baseline/group_diff -d $MODEL_DIR/lpe/model1_lpe.mat -t $MODEL_DIR/design.con -n 1000 -T -v 5
+randomise -i $MODEL_DIR/junk/all_l2_model1_junk.nii.gz -o $MODEL_DIR/junk/rand_baseline/group_diff -d $MODEL_DIR/junk/model1_junk.mat -t $MODEL_DIR/design.con -n 1000 -T -v 5
+randomise -i $MODEL_DIR/task_on/all_l2_model1_task_on.nii.gz -o $MODEL_DIR/task_on/rand_baseline/group_diff -d $MODEL_DIR/task_on/model1_task_on.mat -t $MODEL_DIR/design.con -n 1000 -T -v 5
+randomise -i $MODEL_DIR/rt/all_l2_model1_rt.nii.gz -o $MODEL_DIR/rt/rand_baseline/group_diff -d $MODEL_DIR/rt/model1_rt.mat -t $MODEL_DIR/design.con -n 1000 -T -v 5
+randomise -i $MODEL_DIR/var_sen/all_l2_model1_var_sen.nii.gz -o $MODEL_DIR/var_sen/rand_baseline/group_diff -d $MODEL_DIR/var_sen/model1_var_sen.mat -t $MODEL_DIR/design.con -n 1000 -T -v 5
+randomise -i $MODEL_DIR/ev_sen/all_l2_model1_ev_sen.nii.gz -o $MODEL_DIR/ev_sen/rand_baseline/group_diff -d $MODEL_DIR/ev_sen/model1_ev_sen.mat -t $MODEL_DIR/design.con -n 1000 -T -v 5
