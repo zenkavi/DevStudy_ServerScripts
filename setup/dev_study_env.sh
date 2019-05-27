df2458d4ad7514de442d7e742efad5bf33db7c2e
Zeynep Enkavi <zeynepenkavi@gmail.com>
2019-05-26 21:35:48 -0700

    cleanup setup script

--- setup/dev_study_env.sh
+++ setup/dev_study_env.sh
@@ -1,8 +1,8 @@
-module load system
-module load x11
-module load libmng
-source activate dev_study
 export TODO_PATH=/oak/stanford/groups/russpold/data/ds000054/todo
 export SERVER_SCRIPTS=/oak/stanford/groups/russpold/users/zenkavi/DevStudy_ServerScripts
 export DATA_LOC=/oak/stanford/groups/russpold/data/ds000054/0.0.4
 export R_LIB=/home/groups/russpold/software/miniconda/envs/dev_study/lib/R/library
+source activate dev_study
+module load system
+module load x11
+module load libmng
