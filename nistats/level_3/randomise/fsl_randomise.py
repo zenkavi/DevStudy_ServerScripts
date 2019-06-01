5df1262e59e431b783058365efa7f9f5b4585fc5
Zeynep Enkavi <zeynepenkavi@gmail.com>
2019-05-31 21:58:24 -0700

    working on randomise

--- nistats/level_3/randomise/fsl_randomise.py
+++ nistats/level_3/randomise/fsl_randomise.py
@@ -96,43 +96,43 @@ if mnum == "model3":
 
 #save outputs
 if len(randomise_results.outputs.tstat_files)>0:
-    for i in 1:len(randomise_results.outputs.tstat_files):
-        randomise_results.outputs.tstat_files[i].to_filename("%s/rand/rand_%s_%s_tstat%s.nii.gz.nii.gz"%(in_path,mnum, reg, str(i)))
+    for i in range(0,len(randomise_results.outputs.tstat_files)):
+        os.rename(randomise_results.outputs.tstat_files[i], "%s/rand/rand_%s_%s_tstat%s.nii.gz.nii.gz"%(in_path,mnum, reg, str(i+1)))
         print("***********************************************")
         print("Saved tstat_file for: %s %s"%(mnum, reg))
         print("***********************************************")
 
 if len(randomise_results.outputs.fstat_files)>0:
-    for i in 1:len(randomise_results.outputs.fstat_files):
-        randomise_results.outputs.fstat_files[i].to_filename("%s/rand/rand_%s_%s_fstat%s.nii.gz.nii.gz"%(in_path,mnum, reg, str(i)))
+    for i in range(0,len(randomise_results.outputs.fstat_files)):
+        os.rename(randomise_results.outputs.fstat_files[i],"%s/rand/rand_%s_%s_fstat%s.nii.gz.nii.gz"%(in_path,mnum, reg, str(i+1)))
         print("***********************************************")
         print("Saved fstat_file for: %s %s"%(mnum, reg))
         print("***********************************************")
 
 if len(randomise_results.outputs.t_p_files)>0:
-    for i in 1:len(randomise_results.outputs.t_p_files):
-        randomise_results.outputs.t_p_files[i].to_filename("%s/rand/rand_%s_%s_t_p%s.nii.gz.nii.gz"%(in_path,mnum, reg, str(i)))
+    for i in range(0,len(randomise_results.outputs.t_p_files)):
+        os.rename(randomise_results.outputs.t_p_files[i], "%s/rand/rand_%s_%s_t_p%s.nii.gz.nii.gz"%(in_path,mnum, reg, str(i+1)))
         print("***********************************************")
         print("Saved t_p_file for: %s %s"%(mnum, reg))
         print("***********************************************")
 
 if len(randomise_results.outputs.f_p_files)>0:
-    for i in 1:len(randomise_results.outputs.f_p_files):
-        randomise_results.outputs.f_p_files[i].to_filename("%s/rand/rand_%s_%s_f_p%s.nii.gz.nii.gz"%(in_path,mnum, reg, str(i)))
+    for i in range(0,len(randomise_results.outputs.f_p_files)):
+        os.rename(randomise_results.outputs.f_p_files[i],"%s/rand/rand_%s_%s_f_p%s.nii.gz.nii.gz"%(in_path,mnum, reg, str(i+1)))
         print("***********************************************")
         print("Saved f_p_file for: %s %s"%(mnum, reg))
         print("***********************************************")
 
 if len(randomise_results.outputs.t_corrected_p_files_files)>0:
-    for i in 1:len(randomise_results.outputs.t_corrected_p_files_files):
-        randomise_results.outputs.t_corrected_p_files_files[i].to_filename("%s/rand/rand_%s_%s_tfce_corrp_tstat%s.nii.gz.nii.gz"%(in_path,mnum, reg, str(i)))
+    for i in range(0:len(randomise_results.outputs.t_corrected_p_files_files)):
+        os.rename(randomise_results.outputs.t_corrected_p_files_files[i],"%s/rand/rand_%s_%s_tfce_corrp_tstat%s.nii.gz.nii.gz"%(in_path,mnum, reg, str(i+1)))
         print("***********************************************")
         print("Saved t_corrected_p_file for: %s %s"%(mnum, reg))
         print("***********************************************")
 
 if len(randomise_results.outputs.f_corrected_p_files_files)>0:
-    for i in 1:len(randomise_results.outputs.f_corrected_p_files_files):
-        randomise_results.outputs.f_corrected_p_files_files[i].to_filename("%s/rand/rand_%s_%s_tfce_corrp_fstat%s.nii.gz.nii.gz"%(in_path,mnum, reg, str(i)))
+    for i in range(0:len(randomise_results.outputs.f_corrected_p_files_files)):
+        os.rename(randomise_results.outputs.f_corrected_p_files_files[i],"%s/rand/rand_%s_%s_tfce_corrp_fstat%s.nii.gz.nii.gz"%(in_path,mnum, reg, str(i+1)))
         print("***********************************************")
         print("Saved t_corrected_p_file for: %s %s"%(mnum, reg))
         print("***********************************************")
