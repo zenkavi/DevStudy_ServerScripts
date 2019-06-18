c5468e79dcee9db7adfecb71a3b5469c85493f40
Zeynep Enkavi <zeynepenkavi@gmail.com>
2019-06-18 11:19:07 -0700

    debug

--- utils/mni2vox.py
+++ utils/mni2vox.py
@@ -4,7 +4,7 @@ def mni2vox(mni, T):
     mni_copy = copy(mni)
     mni_copy.extend([1])
     T = np.transpose(np.linalg.inv(T))
-    vox = np.round(np.matmul(mni,T)[:-1])
+    vox = np.round(np.matmul(mni_copy,T)[:-1])
     vox = [int(x) for x in vox]
     vox = tuple(vox)
     return vox
