af47e332ae64cdbc197072bb3786ab3531ea9b3a
Zeynep Enkavi <zeynepenkavi@gmail.com>
2019-06-18 10:16:22 -0700

    debug

--- utils/mni2vox.py
+++ utils/mni2vox.py
@@ -2,12 +2,9 @@ import numpy as np
 
 def mni2vox(mni, T):
     mni.extend([1])
-    print(mni)
-    print(T)
     T = np.transpose(np.linalg.inv(T))
-    print(T)
-    #coordinate = np.round(np.matmul(mni,T)[:-1])
-    #return coordinate
+    coordinate = np.round(np.matmul(mni,T)[:-1])
+    return coordinate
 
 
 #Translated from http://www.alivelearn.net/?p=1434
