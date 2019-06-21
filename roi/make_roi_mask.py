3cb4c807d634c35ee8fb4cc7f1d1d9c9fd880764
Zeynep Enkavi <zeynepenkavi@gmail.com>
2019-06-21 15:45:06 -0700

    working on rois

--- roi/make_roi_mask.py
+++ roi/make_roi_mask.py
@@ -5,22 +5,31 @@ from nipype.interfaces.fsl import ExtractROI
 import os
 import pandas as pd
 
-data_loc = os.environ[DATA_LOC]
-server_scripts = os.environ[SERVER_SCRIPTS]
+data_loc = os.environ["DATA_LOC"]
+server_scripts = os.environ["SERVER_SCRIPTS"]
 out_path = os.path.join(data_loc, 'derivatives', 'rois')
 anatfile = '~/datalad/templateflow/tpl-MNI152NLin2009cAsym/tpl-MNI152NLin2009cAsym_res-01_desc-brain_T1w.nii.gz'
+space = os.path.basename(anatfile)
+space = os.path.splitext(os.path.splitext(space)[0])[0]
 centers = pd.read_csv(os.path.join(server_scripts, 'roi', 'sv_roi_centers.csv'))
+size = 5
 
 for i in range(0, center.shape[0]):
 
-    out_file = ...
-    ctr = ...
-    
+    cur_row = centers.iloc[i]
+    out_file_name = os.path.join(out_path, space, cur_row['Name'])
+    if not os.path.exists(os.path.dirname(out_file_name)):
+        os.makedirs(os.path.dirname(out_file_name))
+
     fslroi = ExtractROI(in_file=anatfile,
-                        roi_file = out_file,
-                        x_min=ctr[0],
-                        x_size=5,
-                        y_min=ctr[1],
-                        y_size=5,
-                        z_min=ctr[2],
-                        z_size=5)
+                        roi_file = out_file_name,
+                        x_min=cur_row.x,
+                        x_size=size,
+                        y_min=cur_row.y,
+                        y_size=size,
+                        z_min=cur_row.z,
+                        z_size=size)
+
+    print("***********************************************")
+    print("ROI saved in %s"%(out_file_name))
+    print("***********************************************")
