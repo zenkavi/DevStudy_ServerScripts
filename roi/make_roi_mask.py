720a67f762533f163e0de6f403f929842069ab80
Zeynep Enkavi <zeynepenkavi@gmail.com>
2019-06-21 16:19:28 -0700

    debug

--- roi/make_roi_mask.py
+++ roi/make_roi_mask.py
@@ -8,16 +8,16 @@ import pandas as pd
 data_loc = os.environ["DATA_LOC"]
 server_scripts = os.environ["SERVER_SCRIPTS"]
 out_path = os.path.join(data_loc, 'derivatives', 'rois')
-anatfile = '~/datalad/templateflow/tpl-MNI152NLin2009cAsym/tpl-MNI152NLin2009cAsym_res-01_desc-brain_T1w.nii.gz'
+anatfile = '/home/users/zenkavi/datalad/templateflow/tpl-MNI152NLin2009cAsym/tpl-MNI152NLin2009cAsym_res-01_desc-brain_T1w.nii.gz'
 space = os.path.basename(anatfile)
 space = os.path.splitext(os.path.splitext(space)[0])[0]
 centers = pd.read_csv(os.path.join(server_scripts, 'roi', 'sv_roi_centers.csv'))
 size = 5
 
-for i in range(0, center.shape[0]):
+for i in range(0, centers.shape[0]):
 
     cur_row = centers.iloc[i]
-    out_file_name = os.path.join(out_path, space, cur_row['Name'])
+    out_file_name = os.path.join(out_path, space, cur_row['Name']+'.nii.gz')
     if not os.path.exists(os.path.dirname(out_file_name)):
         os.makedirs(os.path.dirname(out_file_name))
 
@@ -29,6 +29,7 @@ for i in range(0, center.shape[0]):
                         y_size=size,
                         z_min=cur_row.z,
                         z_size=size)
+    fslroi.run()
 
     print("***********************************************")
     print("ROI saved in %s"%(out_file_name))
