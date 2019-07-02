95bf3236e80058d8cf4f46788356509dde242a46
Zeynep Enkavi <zeynepenkavi@gmail.com>
2019-07-02 14:28:47 -0700

    cleanup

--- roi/make_roi_mask.py
+++ roi/make_roi_mask.py
@@ -23,16 +23,11 @@ for i in range(0, centers.shape[0]):
     if not os.path.exists(os.path.join(out_path, space)):
         os.makedirs(os.path.join(out_path, space))
 
-    #fslmaths $FSLDIR/data/standard/MNI152_T1_2mm.nii.gz -mul 0 -add 1 -roi 44 1 86 1 32 1 0 1 VMPFC_point -odt float
-    #fslmaths VMPFC_point.nii.gz -kernel sphere 5 -fmean VMPFC_sphere -odt float
-    #fslmaths VMPFC_sphere.nii.gz -bin VMPFC_bin.nii.gz
-
     maths1 = fsl.ImageMaths()
     maths1.inputs.in_file = anatfile
     maths1.inputs.op_string = '-mul 0 -add 1 -roi %s 1 %s 1 %s 1 0 1'%(vox_cor[0], vox_cor[1], vox_cor[2])
     maths1.inputs.out_data_type = 'float'
     maths1.inputs.out_file = os.path.join(out_path, space, cur_row['Name']+'_point.nii.gz')
-    #maths1.cmdline
     maths1.run()
 
     maths2 = fsl.ImageMaths()
@@ -40,14 +35,12 @@ for i in range(0, centers.shape[0]):
     maths2.inputs.op_string = '-kernel sphere 5 -fmean'
     maths2.inputs.out_data_type = 'float'
     maths2.inputs.out_file = os.path.join(out_path, space, cur_row['Name']+'_sphere.nii.gz')
-    maths2.cmdline
     maths2.run()
 
     maths3 = fsl.ImageMaths()
     maths3.inputs.in_file = os.path.join(out_path, space, cur_row['Name']+'_sphere.nii.gz')
     maths3.inputs.op_string = '-bin'
     maths3.inputs.out_file = os.path.join(out_path, space, cur_row['Name']+'_bin.nii.gz')
-    maths3.cmdline
     maths3.run()
 
     print("***********************************************")
