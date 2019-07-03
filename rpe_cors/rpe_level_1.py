4fa9cf8f339b8386f1d9293e96f77922681ea6d3
Zeynep Enkavi <zeynepenkavi@gmail.com>
2019-07-03 15:49:04 -0700

    refactoring level1 s to run using all predicted pe s

--- rpe_cors/rpe_level_1.py
+++ rpe_cors/rpe_level_1.py
@@ -2,7 +2,7 @@ from argparse import ArgumentParser
 import os
 import sys
 sys.path.append(os.path.join(os.environ['SERVER_SCRIPTS'],'nistats/level_1'))
-from level_1 import run_level1
+from level_1_utils import make_contrasts, add_transform, stdize, get_conditions, get_confounds, run_level1
 #Usage: python rpe_level_1.py -s SUBNUM
 
 parser = ArgumentParser()
@@ -14,11 +14,10 @@ out_path = args.out_path
 data_loc = os.environ['DATA_LOC']
 server_scripts = os.environ['SERVER_SCRIPTS']
 
-
 pred_rpes = glob.glob(os.path.join(server_scripts, 'rpe_cors/pred_rpes'))
 
 for cur_pes in pred_rpes:
     print("***********************************************")
     print("Running level 1 for sub-%s model %s"%(subnum, os.path.basename(cur_pes)))
     print("***********************************************")
-    run_level1(subnum = subnum, out_path = out_path, pe = True, pe_path = cur_pes)
+    run_level1(subnum = subnum, out_path = out_path, pe = True, pe_path = cur_pes, beta = True)
