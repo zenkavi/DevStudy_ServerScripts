f3d65eee5eb78a875f3f6cac9c3c1f6e75cec73d
Zeynep Enkavi <zeynepenkavi@gmail.com>
2019-07-04 10:57:01 -0700

    debug

--- rpe_cors/get_vStr_betas.py
+++ rpe_cors/get_vStr_betas.py
@@ -1,14 +1,17 @@
 import glob
 import numpy as np
 import nibabel as nib
-import nilearn
+import nilearn.plotting
 import os
 import pandas as pd
 import re
 
+data_loc = os.environ['DATA_LOC']
+server_scripts = os.environ['SERVER_SCRIPTS']
+
 #contrasts = hpe, lpe, pe
 
-regions = ['lvStr', 'rvStr']
+regions = ['l_vstr', 'r_vstr']
 
 models = glob.glob(os.path.join(server_scripts, 'rpe_cors/pred_rpes/*.csv'))
 models = [os.path.splitext(os.path.basename(x))[0] for x in models]
@@ -40,12 +43,12 @@ for model in models:
                 roi_data = np.where(mask_data == 1,img_data,0)
                 roi_data = roi_data[roi_data != 0]
                 cur_betas = pd.DataFrame()
+                cur_betas["beta"] = roi_data
                 cur_betas["sub_num"] = re.findall('\d+', os.path.basename(cur_beta_img))[0] #take from cur_beta_img
                 cur_betas["run_num"] = re.findall('\d+', os.path.basename(cur_beta_img))[1] #take from cur_beta_img
                 cur_betas["pe_type"] = os.path.basename(cur_beta_img).split("_")[2] #take from cur_beta_img
                 cur_betas["roi"] = region
                 cur_betas["model"] = model
-                cur_betas["beta"] = roi_data
                 all_betas = all_betas.append(cur_betas, ignore_index= True)
 
-all_betas.to_csv('%s/derivatives/rpe_cors/all_vstr_betas.csv'%(data_loc))
+all_betas.to_csv('%s/derivatives/rpe_cors/all_vstr_pe_betas.csv'%(data_loc))
