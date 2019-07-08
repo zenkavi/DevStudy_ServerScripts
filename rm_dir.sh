6eae0c4aba509d0744e816e107a2b3de7aa80357
Zeynep Enkavi <zeynepenkavi@gmail.com>
2019-07-07 22:03:59 -0700

    cleanup

--- rm_dir.sh
+++ rm_dir.sh
@@ -1,3 +1,1 @@
-rsync -a --delete /oak/stanford/groups/russpold/data/ds000054/empty_dir/ /oak/stanford/groups/russpold/data/ds000054/0.0.1/
-rsync -a --delete /oak/stanford/groups/russpold/data/ds000054/empty_dir/ /oak/stanford/groups/russpold/data/ds000054/0.0.2/
-rsync -a --delete /oak/stanford/groups/russpold/data/ds000054/empty_dir/ /oak/stanford/groups/russpold/data/ds000054/0.0.3/
+rsync -a --delete /oak/stanford/groups/russpold/data/ds000054/empty_dir/ /oak/stanford/groups/russpold/data/ds000054/0.0.4/derivatives/fmriprep_1.3.0/
