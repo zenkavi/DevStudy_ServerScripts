a937d0657223a7a5ff42e54069e40243f8a3d0ec
Zeynep Enkavi <zeynepenkavi@gmail.com>
2019-07-12 12:09:13 -0700

    added extracting zvals

---
+++ roi/get_all_roi_zvals_task_list.sh
@@ -0,0 +1,16 @@
+./get_all_roi_vals.py -l l_vstr -r hpe -v zval
+./get_all_roi_vals.py -l r_vstr -r hpe -v zval
+./get_all_roi_vals.py -l acc -r hpe -v zval
+./get_all_roi_vals.py -l l_ains -r hpe -v zval
+./get_all_roi_vals.py -l pcc -r hpe -v zval
+./get_all_roi_vals.py -l pre_sma -r hpe -v zval
+./get_all_roi_vals.py -l r_ains -r hpe -v zval
+./get_all_roi_vals.py -l vmpfc -r hpe -v zval
+./get_all_roi_vals.py -l l_vstr -r lpe -v zval
+./get_all_roi_vals.py -l r_vstr -r lpe -v zval
+./get_all_roi_vals.py -l acc -r lpe -v zval
+./get_all_roi_vals.py -l l_ains -r lpe -v zval
+./get_all_roi_vals.py -l pcc -r lpe -v zval
+./get_all_roi_vals.py -l pre_sma -r lpe -v zval
+./get_all_roi_vals.py -l r_ains -r lpe -v zval
+./get_all_roi_vals.py -l vmpfc -r lpe -v zval
