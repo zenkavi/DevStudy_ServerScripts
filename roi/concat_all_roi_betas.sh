ebfa205b3075b3b6d2f3ffcb3bc02cb11cd15785
Zeynep Enkavi <zeynepenkavi@gmail.com>
2019-07-12 13:44:28 -0700

    added concat script

--- roi/concat_all_roi_betas.sh
+++ roi/concat_all_roi_betas.sh
@@ -1,5 +1,5 @@
 cd /oak/stanford/groups/russpold/data/ds000054/0.0.4/derivatives/rois/tpl-MNI152NLin2009cAsym_res-02_desc-brain_T1w
-cat *.csv > /oak/stanford/groups/russpold/data/ds000054/0.0.4/derivatives/rois/tpl-MNI152NLin2009cAsym_res-02_desc-brain_T1w/all_roi_pe_betas.csv
+cat *betas.csv > /oak/stanford/groups/russpold/data/ds000054/0.0.4/derivatives/rois/tpl-MNI152NLin2009cAsym_res-02_desc-brain_T1w/all_roi_pe_betas.csv
 awk '!a[$0]++' all_roi_pe_betas.csv > all_roi_pe_betas_clean.csv
 rm all_roi_pe_betas.csv
 mv all_roi_pe_betas_clean.csv ./all_roi_pe_betas.csv
