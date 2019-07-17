fe870238215307c7fca32650a584679af8d9137f
Zeynep Enkavi <zeynepenkavi@gmail.com>
2019-07-17 16:41:15 -0700

    expanding roi analyses

---
+++ roi/get_all_roi_m1_betas_task_list.sh
@@ -0,0 +1,8 @@
+./get_all_roi_vals.py -l l_vstr -r m1 -v beta
+./get_all_roi_vals.py -l r_vstr -r m1 -v beta
+./get_all_roi_vals.py -l acc -r m1 -v beta
+./get_all_roi_vals.py -l l_ains -r m1 -v beta
+./get_all_roi_vals.py -l pcc -r m1 -v beta
+./get_all_roi_vals.py -l pre_sma -r m1 -v beta
+./get_all_roi_vals.py -l r_ains -r m1 -v beta
+./get_all_roi_vals.py -l vmpfc -r m1 -v beta
