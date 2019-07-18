bd03053c28d9b393113a14b05da7385a0ad4f6fa
Zeynep Enkavi <zeynepenkavi@gmail.com>
2019-07-18 09:08:03 -0700

    added pred evs

--- rpe_cors/save_pred_regs_for_fmri.R
+++ rpe_cors/save_pred_regs_for_fmri.R
@@ -1,29 +1,27 @@
 input_path = '/Users/zeynepenkavi/Dropbox/PoldrackLab/DevStudy_Analyses/input/'
 
+source('/Users/zeynepenkavi/Dropbox/PoldrackLab/DevStudy_Analyses/code/helper_functions/rbind_all_columns.R')
+
 preds = list.files(path=paste0(input_path, "rl_preds/"), pattern = "All")
 
-all_mods_pes = data.frame()
+all_mods_preds = data.frame()
 
 for(f in preds){
   data = read.csv(paste0(input_path, "rl_preds/", f))
-  data = data %>% select(PE, sub_id, X, model)
-  names(data)[which(names(data)=="PE")] = paste0("PE_",unique(data$model))
-  data = data %>% select(-model)
-  if(nrow(all_mods_pes)==0){
-    all_mods_pes = data
-  }
-  else{
-    all_mods_pes = all_mods_pes %>%
-      left_join(data, by=c("sub_id", "X"))
-  }
+  all_mods_preds = rbind.all.columns(all_mods_preds,data.frame(data))
 }
-
-all_mods_pes = all_mods_pes %>%
-  select(X, sub_id, everything())
 
 rm(data)
 
-names(all_mods_pes) = gsub("PE_Preds_", "", names(all_mods_pes))
+all_mods_preds$model = gsub("Preds_", "", all_mods_preds$model)
+
+all_mods_pes = all_mods_preds %>%
+  select(X, PE, sub_id, model) %>%
+  spread(model, PE)
+
+all_mods_evs = all_mods_preds %>%
+  select(X, EV, sub_id, model) %>%
+  spread(model, EV)
 
 exc_models = c("Fit_alpha-beta-exp_Fix_lossave_", "Fit_alpha-beta-exp-lossave_Fix_", "Fit_alpha-beta_Fix_exp-lossave_")
 
@@ -33,7 +31,7 @@ preds = preds[preds %in% exc_models==FALSE]
 
 preds = c(preds, "exp_exp", "alpha_alpha", "exp_alpha")
 
-get_fmri_pe = function(sub_data, model){
+get_fmri_reg = function(sub_data, model, type="pe"){
   
   if(model == "exp_exp"){
     model = c("Fit_alpha-beta-exp_neg-exp_pos-lossave_Fix_",
@@ -76,25 +74,48 @@ get_fmri_pe = function(sub_data, model){
   rem_cols = setdiff(model, drop_cols)
   
   if(length(rem_cols)>0){
-    sub_data = sub_data %>%
-      mutate(PE = rowSums(select(., rem_cols))/length(rem_cols)) %>%
-      select(X, sub_id, PE) %>%
-      filter(is.na(PE) == FALSE)
-    
-    
+    if(type == "pe"){
+      sub_data = sub_data %>%
+        mutate(PE = rowSums(select(., rem_cols))/length(rem_cols)) %>%
+        select(X, sub_id, PE) %>%
+        filter(is.na(PE) == FALSE)
+    }
+    if(type == "ev"){
+      sub_data = sub_data %>%
+        mutate(EV = rowSums(select(., rem_cols))/length(rem_cols)) %>%
+        select(X, sub_id, EV)
+    }
   } else{
-    sub_data = data.frame(X=NA, sub_id = unique(sub_data$sub_id), PE=NA)
+    if(type == "pe"){
+      sub_data = data.frame(X=NA, sub_id = unique(sub_data$sub_id), PE=NA)
+    }
+    if(type == "ev"){
+      sub_data = data.frame(X=NA, sub_id = unique(sub_data$sub_id), EV=NA)
+    }
   }
   return(sub_data)
 }
 
+type = "ev"
+
 for(i in 1:length(preds)){
-  fmri_pes = all_mods_pes %>%
-    group_by(sub_id) %>%
-    do(get_fmri_pe(., model = preds[i])) %>%
-    filter(is.na(PE) == FALSE)
   
-  write.csv(fmri_pes, paste0('/Users/zeynepenkavi/Dropbox/PoldrackLab/DevStudy_ServerScripts/rpe_cors/pred_rpes/',preds[i],'.csv'), row.names=FALSE)
+  if(type == "pe"){
+    fmri_pes = all_mods_pes %>%
+      group_by(sub_id) %>%
+      do(get_fmri_reg(., model = preds[i], type="pe")) %>%
+      filter(is.na(PE) == FALSE)
+    
+    write.csv(fmri_pes, paste0('/Users/zeynepenkavi/Dropbox/PoldrackLab/DevStudy_ServerScripts/rpe_cors/pred_rpes/',preds[i],'.csv'), row.names=FALSE) 
+  }
+  if(type == "ev"){
+    fmri_evs = all_mods_evs %>%
+      group_by(sub_id) %>%
+      do(get_fmri_reg(., model = preds[i], type="ev")) %>%
+      mutate(EV=ifelse(is.na(EV), 0, EV))
+    
+    write.csv(fmri_evs, paste0('/Users/zeynepenkavi/Dropbox/PoldrackLab/DevStudy_ServerScripts/rpe_cors/pred_evs/',preds[i],'.csv'), row.names=FALSE) 
+  }
   
 }
 
