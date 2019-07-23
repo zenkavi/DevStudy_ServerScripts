12f7f4d3bced84937cb018156380af74e1eef32e
Zeynep Enkavi <zeynepenkavi@gmail.com>
2019-07-23 10:38:22 -0700

    added new repo to setup

--- setup/dev_study_env.sh
+++ setup/dev_study_env.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 export TODO_PATH=/oak/stanford/groups/russpold/data/ds000054/todo
 export SERVER_SCRIPTS=/oak/stanford/groups/russpold/users/zenkavi/DevStudy_ServerScripts
+export NOTEBOOKS=/oak/stanford/groups/russpold/users/zenkavi/DevStudy_fmri-nbs
 export DATA_LOC=/oak/stanford/groups/russpold/data/ds000054/0.0.4
 export R_LIB=/home/groups/russpold/software/miniconda/envs/dev_study/lib/R/library
 source activate dev_study
