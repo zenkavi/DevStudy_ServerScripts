27e2a5fd5c139b4ab9db123ae15ad4d117617e43
Zeynep Enkavi <zeynepenkavi@gmail.com>
2019-07-30 19:11:15 -0700

    cont working on ppi

--- func_con/ppi/ppi_level_1.py
+++ func_con/ppi/ppi_level_1.py
@@ -1,9 +1,9 @@
 #!/home/groups/russpold/software/miniconda/envs/fmri/bin/python
 from argparse import ArgumentParser
+import os
 import sys
 sys.path.append(os.path.join(os.environ['SERVER_SCRIPTS'], '/nistats/level_1'))
 from level_1_utils import run_ppi_level1
-import os
 #Usage: python level_1.py -s SUBNUM -pe
 
 parser = ArgumentParser()
