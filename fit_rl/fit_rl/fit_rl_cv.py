291e6744fa356e2e794103e20da5b8b51eb083a1
Zeynep Enkavi <zeynepenkavi@gmail.com>
2019-09-18 14:28:59 -0700

    debug

--- fit_rl/fit_rl/fit_rl_cv.py
+++ fit_rl/fit_rl/fit_rl_cv.py
@@ -10,6 +10,7 @@ from scipy.stats import truncnorm
 from argparse import ArgumentParser
 from helper_funcs.select_optimal_parameters import select_optimal_parameters
 from helper_funcs.get_predicted_df import get_predicted_df
+from helper_funcs.extract_pars import get_model_name
 
 todo_path = os.environ['TODO_PATH']
 server_scripts = os.environ['SERVER_SCRIPTS']
@@ -19,7 +20,7 @@ parser.add_argument("-s", "--subject", help="subject number")
 parser.add_argument("-n", "--n_fits", default=50, help="Number of iterations for model")
 parser.add_argument("-dp", "--data_path", default=todo_path+'/behav_data_tb_organized/machine_game/' , help="data path")
 parser.add_argument("-op", "--output_path", default=server_scripts+'/fit_rl/.cv_fits/', help="output path")
-parser.add_argument("-f", "--folds", default=4, help="number of cv folds")
+parser.add_argument("-f", "--fold_nums", default=4, help="number of cv folds")
 parser.add_argument("-p", "--pars", help="parameters dictionary")
 args = parser.parse_args()
 
@@ -27,7 +28,7 @@ args = parser.parse_args()
 subject = args.subject
 n_fits = args.n_fits
 data_path = args.data_path
-folds = int(args.folds)
+fold_nums = int(args.fold_nums)
 if not os.path.exists(output_path):
     os.makedirs(output_path)
 pars = args.pars
@@ -38,8 +39,9 @@ data =  pd.read_csv(data_path+'ProbLearn'+str(subject)+'.csv')
 model_name = get_model_name(pars)
 
 #assign cv folds to subject data (in this case try 25% vs 75% so 4 fold)
-fold_nums = list(range(1,fold_nums+1))*data.shape[0]/fold_nums
-random.seed(42354)
+fold_nums = list(range(1,fold_nums+1))*int(data.shape[0]/fold_nums)
+r_seed = random.randint(1000, 9999999)
+random.seed(r_seed)
 random.shuffle(fold_nums)
 if len(fold_nums) != data.shape[0]:
     fold_nums = fold_nums[:data.shape[0]]
@@ -51,8 +53,8 @@ for cur_fold in range(1,fold_nums+1):
     print('Running fold: %s for subject: %s'%(str(cur_fold), subject))
 
     #slice data
-    train_data = data.query("fold_nums != cur_fold")
-    test_data = data.query("fold_nums == cur_fold")
+    train_data = data[data.fold_nums != cur_fold]
+    test_data = data[data.fold_nums == cur_fold]
 
     #get parameters
     opt_pars_dict = select_optimal_parameters(data=train_data, subject=subject, n_fits=n_fits, pars = pars)
@@ -64,6 +66,7 @@ for cur_fold in range(1,fold_nums+1):
     fold_out = pd.DataFrame.from_dict(opt_pars_dict)
     fold_out = df.add_prefix('xopt_')
     fold_out['fold'] =  cur_fold
+    fold_out['seed'] = r_seed
     fold_out['sub_id'] = subject
     fold_out['pred_acc'] = sum(pred_df['pred_correct'])/pred_df.shape[0]
     fold_out['model_name'] = model_name
