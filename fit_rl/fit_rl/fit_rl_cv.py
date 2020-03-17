af6b2b67343e858f7a9922c177b5d58dff7c9187
Zeynep Enkavi <zeynepenkavi@gmail.com>
2020-03-17 16:57:35 -0700

    started working on improving behavioral parameter estimates

--- fit_rl/fit_rl/fit_rl_cv.py
+++ fit_rl/fit_rl/fit_rl_cv.py
@@ -5,10 +5,9 @@ import numpy as np
 import os
 import pandas as pd
 import random
-import scipy.optimize
 from scipy.stats import truncnorm
 from argparse import ArgumentParser
-from helper_funcs.select_optimal_parameters import select_optimal_parameters
+from .fit_rl_mcmc import fit_rl_mcmc
 from helper_funcs.get_predicted_df import get_predicted_df
 from helper_funcs.extract_pars import get_model_name
 
@@ -17,7 +16,6 @@ server_scripts = os.environ['SERVER_SCRIPTS']
 
 parser = ArgumentParser()
 parser.add_argument("-s", "--subject", help="subject number")
-parser.add_argument("-n", "--n_fits", default=50, help="Number of iterations for model")
 parser.add_argument("-dp", "--data_path", default=todo_path+'/behav_data_tb_organized/machine_game/' , help="data path")
 parser.add_argument("-op", "--output_path", default=server_scripts+'/fit_rl/.cv_fits/', help="output path")
 parser.add_argument("-f", "--fold_nums", default=4, help="number of cv folds")
@@ -26,7 +24,7 @@ args = parser.parse_args()
 
 #initialize arguments
 subject = args.subject
-n_fits = args.n_fits
+
 data_path = args.data_path
 fold_nums = int(args.fold_nums)
 output_path = args.output_path
@@ -40,6 +38,9 @@ data =  pd.read_csv(data_path+'ProbLearn'+str(subject)+'.csv')
 model_name = get_model_name(pars)
 
 #assign cv folds to subject data (in this case try 25% vs 75% so 4 fold)
+
+#CHANGE THIS TO BALANCED FOLDS!
+
 fold_nums_col = list(range(1,fold_nums+1))*int(data.shape[0]/fold_nums)
 r_seed = random.randint(1000, 9999999)
 random.seed(r_seed)
@@ -66,7 +67,8 @@ for cur_fold in range(1,fold_nums+1):
         test_data = data
 
     #get parameters
-    opt_pars_dict = select_optimal_parameters(data=train_data, subject=subject, n_fits=n_fits, pars = pars)
+    #opt_pars_dict = select_optimal_parameters(data=train_data, subject=subject, n_fits=n_fits, pars = pars)
+    opt_pars_dict = fit_rl_mcmc(data=train_data, subject=subject, pars = pars)
 
     #make prediction
     pred_df = get_predicted_df(data=test_data, pars_dict=opt_pars_dict)
---
+++ fit_rl/fit_rl/fit_rl_mcmc.py
@@ -0,0 +1,24 @@
+# Change select optimal parameters to using MCMC through pymc
+
+# Steps: CV MCMC
+    # Split data into k folds with balanced conditions
+    # Fit model and estimate parameters using MCMC for the train data of the fold
+        # Store full posterior distributions for each parameter (e.g. alpha_f1)
+    # Get median of posterior distributions for each parameter in fold
+    # Use these 'best estimates' for the fold to generate predicted choice vector for fold's test data
+    # Compare predicted choice vector to true choices and calculate accuracy
+        # Store each fold's prediction accuracy (e.g. pred_acc_f1)
+
+#Useful resource: https://github.com/ricardoV94/stats/blob/master/modelling/RL_PyMC.ipynb
+
+# Desired output:
+# Per subject -
+# Mean accuracy across folds
+# Variance of accuracy across folds
+# Posteriors for each parameter across folds
+
+# Run for each subject for each model?
+# Input: Subject data
+
+import pymc3 as pm
+import theano.tensor as tt
